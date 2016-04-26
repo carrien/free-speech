b8ae2c427476e8bf5e88850dc0c4cfb4607a3cc7
Carrie Niziolek <carrien@bu.edu>
2016-04-26 02:49:30 -0400

    Updated for new fmtMatrix format

--- speech/merge_fmtMatrices_crossSubj.m
+++ speech/merge_fmtMatrices_crossSubj.m
@@ -13,58 +13,36 @@ analyses = fieldnames(ffx); %#ok<NODEF>
 for a = 1:length(fns)
     anl = analyses{a};
     for c2m = 1:length(conds2merge)
+        oldcnd1 = conds2merge{c2m}{1};
+        oldcnd2 = conds2merge{c2m}{2};
+        newcnd = mergednames{c2m};
         %ffx
-        merge1 = ffx.(anl).(conds2merge{c2m}{1});
-        merge2 = ffx.(anl).(conds2merge{c2m}{2});
-        merge1mean = ffx_mean.(anl).(conds2merge{c2m}{1});
-        merge2mean = ffx_mean.(anl).(conds2merge{c2m}{2});
-        merge1ci = ffx_ci.(anl).(conds2merge{c2m}{1});
-        merge2ci = ffx_ci.(anl).(conds2merge{c2m}{2});
+        merge1 = ffx.(anl).(oldcnd1);
+        merge2 = ffx.(anl).(oldcnd2);
         if size(merge1,1) == size(merge2,1)
-            ffx.(anl).(mergednames{c2m}) = [merge1 merge2];
-            ffx_mean.(anl).(mergednames{c2m}) = nanmean([merge1mean merge2mean],2);
-            ffx_ci.(anl).(mergednames{c2m}) = nanmean([merge1ci merge2ci],2);
+            ffx.(anl).(newcnd) = [merge1 merge2];
         else
-            ffx.(anl).(mergednames{c2m}) = merge1;
+            ffx.(anl).(newcnd) = merge1;
             for i=1:size(merge2,2)
-                ffx.(anl).(mergednames{c2m}) = nancat(ffx.(anl).(mergednames{c2m}),merge2(:,i));
+                ffx.(anl).(newcnd) = nancat(ffx.(anl).(newcnd),merge2(:,i));
             end
-            ffx_mean.(anl).(mergednames{c2m}) = nanmean(nancat(merge1mean,merge2mean),2);
-            ffx_ci.(anl).(mergednames{c2m}) = nanmean(nancat(merge1ci,merge2ci),2);
         end
-        ffx.(anl) = rmfield(ffx.(anl),conds2merge{c2m}{1});
-        ffx.(anl) = rmfield(ffx.(anl),conds2merge{c2m}{2});
-        ffx_mean.(anl) = rmfield(ffx_mean.(anl),conds2merge{c2m}{1});
-        ffx_mean.(anl) = rmfield(ffx_mean.(anl),conds2merge{c2m}{2});
-        ffx_ci.(anl) = rmfield(ffx_ci.(anl),conds2merge{c2m}{1});
-        ffx_ci.(anl) = rmfield(ffx_ci.(anl),conds2merge{c2m}{2});
+        ffx.(anl) = rmfield(ffx.(anl),oldcnd1);
+        ffx.(anl) = rmfield(ffx.(anl),oldcnd2);
         
         %rfx
-        merge1 = rfx.(anl).(conds2merge{c2m}{1});
-        merge2 = rfx.(anl).(conds2merge{c2m}{2});
-        merge1mean = rfx_mean.(anl).(conds2merge{c2m}{1});
-        merge2mean = rfx_mean.(anl).(conds2merge{c2m}{2});
-        merge1ci = rfx_ci.(anl).(conds2merge{c2m}{1});
-        merge2ci = rfx_ci.(anl).(conds2merge{c2m}{2});
+        merge1 = rfx.(anl).(oldcnd1);
+        merge2 = rfx.(anl).(oldcnd2);
         if size(merge1,1) == size(merge2,1)
-            rfx.(anl).(mergednames{c2m}) = [merge1 merge2];
-            rfx_mean.(anl).(mergednames{c2m}) = nanmean([merge1mean merge2mean],2);
-            rfx_ci.(anl).(mergednames{c2m}) = nanmean([merge1ci merge2ci],2);
+            rfx.(anl).(newcnd) = [merge1 merge2];
         else
-            rfx.(anl).(mergednames{c2m}) = merge1;
+            rfx.(anl).(newcnd) = merge1;
             for i=1:size(merge2,2)
-                rfx.(anl).(mergednames{c2m}) = nancat(rfx.(anl).(mergednames{c2m}),merge2(:,i));
+                rfx.(anl).(newcnd) = nancat(rfx.(anl).(newcnd),merge2(:,i));
             end
-            rfx_mean.(anl).(mergednames{c2m}) = nanmean(nancat(merge1mean,merge2mean),2);
-            rfx_ci.(anl).(mergednames{c2m}) = nanmean(nancat(merge1ci,merge2ci),2);
         end
-        rfx.(anl) = rmfield(rfx.(anl),conds2merge{c2m}{1});
-        rfx.(anl) = rmfield(rfx.(anl),conds2merge{c2m}{2});
-        rfx_mean.(anl) = rmfield(rfx_mean.(anl),conds2merge{c2m}{1});
-        rfx_mean.(anl) = rmfield(rfx_mean.(anl),conds2merge{c2m}{2});
-        rfx_ci.(anl) = rmfield(rfx_ci.(anl),conds2merge{c2m}{1});
-        rfx_ci.(anl) = rmfield(rfx_ci.(anl),conds2merge{c2m}{2});
-
+        rfx.(anl) = rmfield(rfx.(anl),oldcnd1);
+        rfx.(anl) = rmfield(rfx.(anl),oldcnd2);
     end
 end
 
@@ -72,5 +50,5 @@ end
 savefile = fullfile(dataPath,sprintf('%s_merged.mat',plotfile));
 bSave = savecheck(savefile);
 if bSave,
-    save(savefile,'ffx','ffx_mean','ffx_ci','rfx','rfx_mean','rfx_ci','svec')
+    save(savefile,'ffx','rfx','svec')
 end
