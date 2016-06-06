f9a0e423b45e0e2712cf5b40e149e9163a290878
Carrie Niziolek <carrien@bu.edu>
2016-06-06 00:12:04 -0400

    Update txt2ipa to handle cell arrays

--- speech/txt2ipa.m
+++ speech/txt2ipa.m
@@ -4,26 +4,26 @@ function [ipa] = txt2ipa(txt)
 if iscellstr(txt), txtcell = txt;
 elseif ischar(txt), txtcell = {txt}; % if string, convert to cell array
 else error('Input must be a text string or cell array of strings.')
-end 
+end
 
-    vowels = {'a' 'ae' 'E' 'eI' 'I' 'i' 'o' 'u' 'er'};
-    
-    a = ismember(txtcell,'ah');
-    ae = ismember(txtcell,{'ae' 'add' 'ad' 'rad'});
-    E = ismember(txtcell,{'eh' 'Ed' 'head' 'red' 'glen' 'blend'});
-    eI = ismember(txtcell,{'grain'});
-    I = ismember(txtcell,{'ih' 'I' 'rid' 'grin'});
-    i = ismember(txtcell,{'ee' 'eat' 'reed' 'green' 'bleed'});
-    o = ismember(txtcell,{'oh' 'blow'});
-    u = ismember(txtcell,{'oo' 'rude' 'groom' 'blue'});
-    er = ismember(txtcell,{'er' 'blur'});
-    
-    vowelinds = [a; ae; E; eI; I; i; o; u; er];
-    vowelinds = sum(vowelinds .* repmat([1:size(vowelinds,1)]',1,size(vowelinds,2)),1);
-    if any(~vowelinds)
-        notfound = unique(txtcell(~vowelinds));
-        error('Text ''%s'' not found in IPA conversion table. ',notfound{:});
-    end
-    ipa = vowels(vowelinds);
+vowels = {'a' 'ae' 'E' 'eI' 'I' 'i' 'o' 'u' 'er'};
+
+a = ismember(txtcell,'ah');
+ae = ismember(txtcell,{'ae' 'add' 'ad' 'rad'});
+E = ismember(txtcell,{'eh' 'Ed' 'head' 'red' 'glen' 'blend'});
+eI = ismember(txtcell,{'grain'});
+I = ismember(txtcell,{'ih' 'I' 'rid' 'grin'});
+i = ismember(txtcell,{'ee' 'eat' 'reed' 'green' 'bleed'});
+o = ismember(txtcell,{'oh' 'blow'});
+u = ismember(txtcell,{'oo' 'rude' 'groom' 'blue'});
+er = ismember(txtcell,{'er' 'blur'});
+
+vowelinds = [a; ae; E; eI; I; i; o; u; er];
+vowelinds = sum(vowelinds .* repmat([1:size(vowelinds,1)]',1,size(vowelinds,2)),1);
+if any(~vowelinds)
+    notfound = unique(txtcell(~vowelinds));
+    error('Text ''%s'' not found in IPA conversion table. ',notfound{:});
+end
+ipa = vowels(vowelinds);
 
 if ischar(txt), ipa = ipa{1}; end % if input was string, convert back
