b9755b429b3352e62e7448e45a4ce27d5c7504e3
Carrie Niziolek <carrien@bu.edu>
2016-07-29 17:52:47 -0400

    Added ax arg for plotting on any axes

--- plotting/hline.m
+++ plotting/hline.m
@@ -1,8 +1,10 @@
-function hl = hline(val,color,linestyle)
-% function hl = hline(val,color,linestyle)
+function hl = hline(val,color,linestyle,ax)
+% function hl = hline(val,color,linestyle,ax)
 
-a = axis;
-hl = line(a(1:2),val*ones(2,1));
+if nargin < 4, ax = gca; end
+
+a = axis(ax);
+hl = line(a(1:2),val*ones(2,1),'Parent',ax);
 if nargin >= 2
   set(hl,'Color',color);
 end
--- plotting/vline.m
+++ plotting/vline.m
@@ -1,11 +1,13 @@
-function hl = vline(val,color,linestyle)
-% function hl = vline(val,color,linestyle)
+function hl = vline(val,color,linestyle,ax)
+% function hl = vline(val,color,linestyle,ax)
 
-a = axis;
-hl = line(val*ones(2,1),a(3:4));
-if nargin >= 2
+if nargin < 4, ax = gca; end
+
+a = axis(ax);
+hl = line(val*ones(2,1),a(3:4),'Parent',ax);
+if nargin >= 2 && ~isempty(color)
   set(hl,'Color',color);
 end
-if nargin >= 3
+if nargin >= 3 && ~isempty(linestyle)
   set(hl,'LineStyle',linestyle);
 end
