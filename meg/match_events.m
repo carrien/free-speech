d0ca314f5c0dd00913b0e92a3eac0b933e7beeeb
carrien <carrien@bu.edu>
2017-04-18 22:09:03 -0400

    Bugfixes for matching events

--- meg/match_events.m
+++ meg/match_events.m
@@ -1,6 +1,9 @@
-function [ ] = match_events(expt,allevents,newEventInfo)
+function [ ] = match_events(expt,allevents,newEventInfo,missingTrialsSpeak,missingTrialsListen)
 %MATCH_EVENTS  Match experiment trials with MEG events.
 %   MATCH_EVENTS(EXPT,EVENTS,NEWEVENTINFO)
+
+if nargin < 4, missingTrialsSpeak = []; end
+if nargin < 5, missingTrialsListen = []; end
 
 nwords = length(expt.words);
 eventnames = {allevents.label};
@@ -26,8 +29,15 @@ speak_fileinds = [speak_events.fileind];
 [speak_eventtimes,inds] = sort(speak_eventtimes);
 speak_eventsamples = speak_eventsamples(inds);
 speak_eventorder = speak_eventorder(inds);
-speak_eventorder = speak_eventorder - speak_inds(1) + 1; % convent to 1,2,3
+speak_eventorder = speak_eventorder - speak_inds(1) + 1; % convert to 1,2,3
 speak_fileinds = speak_fileinds(inds);
+for t=1:length(missingTrialsSpeak)
+    tr = missingTrialsSpeak(t);
+    speak_eventtimes = [speak_eventtimes(1:tr-1) NaN speak_eventtimes(tr:end)];
+    speak_eventsamples = [speak_eventsamples(1:tr-1) NaN speak_eventsamples(tr:end)];
+    speak_eventorder = [speak_eventorder(1:tr-1) expt.allWords(tr) speak_eventorder(tr:end)];
+    speak_fileinds = [speak_fileinds(1:tr-1) speak_fileinds(tr) speak_fileinds(tr:end)];
+end
 % listen
 listen_eventtimes = [listen_events.times];
 listen_eventsamples = [listen_events.samples];
@@ -38,28 +48,43 @@ listen_eventsamples = listen_eventsamples(inds);
 listen_eventorder = listen_eventorder(inds);
 listen_eventorder = listen_eventorder - listen_inds(1) + 1; % convert to 1,2,3
 listen_fileinds = listen_fileinds(inds);
+for t=1:length(missingTrialsListen)
+    tr = missingTrialsListen(t);
+    listen_eventtimes = [listen_eventtimes(1:tr-1) NaN listen_eventtimes(tr:end)];
+    listen_eventsamples = [listen_eventsamples(1:tr-1) NaN listen_eventsamples(tr:end)];
+    listen_eventorder = [listen_eventorder(1:tr-1) expt.allWords(tr) listen_eventorder(tr:end)];
+    listen_fileinds = [listen_fileinds(1:tr-1) listen_fileinds(tr) listen_fileinds(tr:end)];
+end
 
 % check that order matches expt file
 if length(expt.allWords) ~= length(speak_eventorder)
     warning('Word and speak event lists do not have the same number of elements. Checking for earliest mismatch.');
-    figure; plot(speak_eventorder,':')
+    figure; plot(speak_eventorder,'ro-')
     hold on;
     plot(expt.allWords,'g'); % ground truth
     legend({'triggers in event order','presented words'});
     % find first mismatching element
     len = min(length(expt.allWords),length(speak_eventorder));
     mismatch = find(expt.allWords(1:len) ~= speak_eventorder(1:len));
-    error('First mismatch found at %d seconds (trial %d)',speak_eventtimes(mismatch(1)),mismatch(1));
+    if isempty(mismatch), mismatch = len+1; end
+    axis([max(0,mismatch(1)-5) min(mismatch(1)+10) .5 3.5]);
+    set(gcf,'Position',[0 50 650 450]);
+    %ax_pos = get(gca,'Position');
+    %axes('Position',ax_pos,'XAxisLocation','top');
+    error('First mismatch found at %.1f seconds (trial %d)',speak_eventtimes(mismatch(1)),mismatch(1));
 elseif length(expt.allWords) ~= length(listen_eventorder)
     warning('Word and listen event lists do not have the same number of elements. Checking for earliest mismatch.');
-    figure; plot(listen_eventorder,':')
+    figure; plot(listen_eventorder,'ro-')
     hold on;
     plot(expt.allWords,'g'); % ground truth
     legend({'triggers in event order','presented words'});
     % find first mismatching element
     len = min(length(expt.allWords),length(listen_eventorder));
     mismatch = find(expt.allWords(1:len) ~= listen_eventorder(1:len));
-    error('First mismatch found at %d seconds (trial %d)',listen_eventtimes(mismatch(1)),mismatch(1));
+    if isempty(mismatch), mismatch = len+1; end
+    axis([max(0,mismatch(1)-5) mismatch(1)+10 .5 3.5]);
+    set(gcf,'Position',[0 50 650 450]);
+    error('First mismatch found at %.1f seconds (trial %d)',listen_eventtimes(mismatch(1)),mismatch(1));
 elseif expt.allWords ~= speak_eventorder
     error('Word and event lists are the same length but do not match!')
 elseif speak_eventorder ~= listen_eventorder
--- meg/save_bstEvents.m
+++ meg/save_bstEvents.m
@@ -1,11 +1,13 @@
-function [ ] = save_bstEvents(dataFiles,outPath)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+function [eventFiles] = save_bstEvents(dataFiles,outPath)
+%SAVE_BSTEVENTS  Exports event markers from a list of Brainstorm raw files.
 
+eventFiles = cell(1,length(dataFiles));
 for i=1:length(dataFiles)
     [~,filename] = fileparts(dataFiles{i});
     data = load(dataFiles{i});
-    events = data.F.events;
-    eventfilename = sprintf('events_%s',filename);
-    save(fullfile(outPath,eventfilename),'events');
+    events = data.F.events; %#ok<NASGU>
+    eventfilename = sprintf('events_%s',filename(11:end));
+    eventFiles{i} = fullfile(outPath,eventfilename);
+    save(eventFiles{i},'events');
+    fprintf('Event file written to %s\n',eventFiles{i});
 end
