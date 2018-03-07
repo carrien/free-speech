1fce52af92ae8260c776cf2defc29d1af76cffed
Carrie Niziolek <cniziolek@wisc.edu>
2018-03-07 15:43:52 -0600

    Made audio check f'n play only good trials by default

--- speech/check_audio_errors.m
+++ speech/check_audio_errors.m
@@ -1,17 +1,26 @@
-function [ ] = check_audio_errors(dataPath,grouping,group,nbtrials)
-%CHECK_AUDIO_ERRORS Summary of this function goes here
-%   Detailed explanation goes here
+function [ ] = check_audio_errors(dataPath,grouping,group,nbtrials,bExcl)
+%CHECK_AUDIO_ERRORS  Listen to groups of trials to check for speech errors.
+%   CHECK_AUDIO_ERRORS(DATAPATH,GROUPING,GROUP,NBTRIALS,BEXCL) 
 
 if nargin < 1 || isempty(dataPath), dataPath = cd; end
 if nargin < 2 || isempty(grouping), grouping = 'words'; end
 if nargin < 3 || isempty(group), group = 'eat'; end
-if nargin < 4 || isempty(nbtrials), nbtrials = 0; end
+if nargin < 4 || isempty(nbtrials), nbtrials = 10; end
+if nargin < 5 || isempty(bExcl), bExcl = 1; end
 
 load(fullfile(dataPath,'data.mat'));
 load(fullfile(dataPath,'expt.mat'));
 
 disp(group);
 trials = expt.inds.(grouping).(group);
+
+if bExcl % remove bad trials
+    load(fullfile(dataPath,'dataVals.mat'));
+    excllog = [dataVals.bExcl];
+    excl = dataVals(excllog).token;
+    trials = setdiff(trials,excl);
+end
+
 for t=1:length(trials)
     trialnum = trials(t);
     disp(trialnum);
