f52bf1e474ce56051694208cb115f5ef60c893f0
Carrie Niziolek <cniziolek@wisc.edu>
2018-05-02 10:46:11 -0500

    Update to get acoust path f'n

--- utils/get_meg_rundirs.m
+++ utils/get_meg_rundirs.m
@@ -1,11 +1,11 @@
-function [rundir] = get_meg_rundirs(exptName,snum,nruns)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [rundir] = get_meg_rundirs(exptName,snum,megsubdir,nruns)
+%GET_MEG_RUNDIRS  Get path to MEG run directories.
 
-if nargin < 3, nruns = 4; end
+if nargin < 3 || isempty(megsubdir), megsubdir = 'meg'; end
+if nargin < 4, nruns = 4; end
 
 for i=1:nruns
     rundirname = sprintf('run%02d',i);
-    rundir{i} = getAcoustSubjPath(exptName,snum,'meg',rundirname);
+    rundir{i} = get_acoustLoadPath(exptName,snum,megsubdir,rundirname);
 end
 
