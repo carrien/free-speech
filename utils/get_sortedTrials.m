8ce52387f9b96b4e47880e450e8d1af18a7138a7
Carrie Niziolek <cniziolek@wisc.edu>
2018-06-25 11:37:01 -0500

    Add second output (sorted filenames)

--- utils/get_sortedTrials.m
+++ utils/get_sortedTrials.m
@@ -1,16 +1,18 @@
-function [sortedTrials] = get_sortedTrials(trialpath)
+function [sortedTrialnums,sortedTrialnames] = get_sortedTrials(trialpath)
 %GET_SORTEDTRIALS  Get sorted list of numbered matfiles in a directory.
 
 w = what(trialpath);
 matFiles = w.mat;
 
-% Strip off '.mat' and sort
+% Strip off '.mat' and sort trial numbers
 filenums = zeros(1,length(matFiles));
 for i = 1:length(matFiles)
     [~, name] = fileparts(matFiles{i});
     filenums(i) = str2double(name);
 end
-sortedTrials = sort(filenums);
+[sortedTrialnums,idx] = sort(filenums);
+
+% Sort trial names in the same order
+sortedTrialnames = matFiles(idx);
 
 end
-
