dc826e61c1e807e6cc4fcffdaa373d23cb84c544
Carrie Niziolek <cniziolek@wisc.edu>
2018-08-14 10:42:43 -0500

    Add support for multi-element grouping variable input.

--- stats/add2anovars.m
+++ stats/add2anovars.m
@@ -15,10 +15,10 @@ function [obs,group] = add2anovars(obs,new_obs,group,varargin)
 %         cnd = conds{c};
 %         for v = 1:length(vowels)
 %             vow = vowels{v};
-%             
+%
 %             newdata = data(sid).(cnd).(vow);
 %             [obs,group] = add2anovars(obs,newdata,group,s,c,v);
-% 
+%
 %         end
 %     end
 % end
@@ -31,7 +31,22 @@ end
 obs = [obs; new_obs];
 
 % add categorical data to each grouping variable
+nObs = length(new_obs);
 for v = 1:length(varargin)
-    group{v} = [group{v}; varargin{v}*ones(length(new_obs),1)];
+    thisgroup = varargin{v};
+    if isnumeric(thisgroup)
+        if length(thisgroup)==1                   % if a number
+            group{v} = [group{v}; thisgroup*ones(nObs,1)];
+        elseif iscolumn(thisgroup) && length(thisgroup)==nObs
+            group{v} = [group{v}; thisgroup];            
+        elseif isrow(thisgroup) && length(thisgroup)==nObs
+            group{v} = [group{v}; thisgroup'];            
+        else
+            error('Length of grouping variable is %d. Grouping variable must either be a single element or have the same number of elements as the data to be added (length:).',length(thisgroup),nObs);
+        end
+    elseif ischar
+        error('String format not yet supported for grouping variables.')
+    elseif iscell
+        error('Cell format not yet supported for grouping variables.')
+    end
 end
-
