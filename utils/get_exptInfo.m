7c76c574812a0b9755ac1fe292e6acdf24d4675f
Carrie Niziolek <cniziolek@wisc.edu>
2018-08-23 09:21:44 -0500

    Updated to get_acoustLoadPath function

--- utils/get_exptInfo.m
+++ utils/get_exptInfo.m
@@ -1,5 +1,5 @@
 function [exptInfo] = get_exptInfo(exptName) %#ok<STOUT>
 % Get expt universals
 
-dataPath = getAcoustSubjPath(exptName);
+dataPath = get_acoustLoadPath(exptName);
 load(fullfile(dataPath,'exptInfo.mat'),'exptInfo');
