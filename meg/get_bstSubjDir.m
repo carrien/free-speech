a2f043d168d76d2baeb74b98ffd0d5d772f429d3
Sarah Bakst <sarah@Sarahs-MacBook-Air.local>
2018-10-16 20:31:04 -0500

    generalized (took out Kiran lab hard-coding)

--- meg/get_bstSubjDir.m
+++ meg/get_bstSubjDir.m
@@ -6,6 +6,6 @@ if isnumeric(sid)
     sid = sprintf('s%02d',sid); % create string from numeric for sid
 end
 
-%dbpath = bst_get('BrainstormDbDir');
-dbpath = '/projectnb/skiran/brainstorm_db';
+dbpath = bst_get('BrainstormDbDir');
+%dbpath = '/projectnb/skiran/brainstorm_db';
 bstSubjDir = fullfile(dbpath,exptName,'data',sid);
