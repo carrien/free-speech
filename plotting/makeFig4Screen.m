da7b2c1bc65230eaac8b3f679a7984f5831b2240
Carrie Niziolek <cniziolek@wisc.edu>
2018-11-08 16:47:33 -0600

    Ensure all LineWidths >= 1

--- plotting/makeFig4Screen.m
+++ plotting/makeFig4Screen.m
@@ -6,3 +6,9 @@ hax.FontName = 'Arial';
 hax.FontSize = 14;
 %set(gcf,'Color','none')
 box off;
+
+for c = 1:length(hax.Children)
+    if isprop(hax.Children(c),'LineWidth') && hax.Children(c).LineWidth < 1
+        hax.Children(c).LineWidth = 1;
+    end
+end
