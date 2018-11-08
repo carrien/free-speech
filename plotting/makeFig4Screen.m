e1670ff5c4bed09fab6f0ee5707daab4b3d5bbe7
Carrie Niziolek <cniziolek@wisc.edu>
2018-11-08 17:35:07 -0600

    Change makeFig4Screen to a function

--- plotting/makeFig4Screen.m
+++ plotting/makeFig4Screen.m
@@ -1,4 +1,8 @@
-hax = gca;
+function [] = makeFig4Screen(hax,bChildLineWidth)
+
+if nargin < 1 || isempty(hax), hax = gca; end
+if nargin < 2 || isempty(bChildLineWidth), bChildLineWidth = 1; end
+
 hax.LineWidth = 1;
 hax.XColor = [0 0 0];
 hax.YColor = [0 0 0];
@@ -7,8 +11,10 @@ hax.FontSize = 14;
 %set(gcf,'Color','none')
 box off;
 
-for c = 1:length(hax.Children)
-    if isprop(hax.Children(c),'LineWidth') && hax.Children(c).LineWidth < 1
-        hax.Children(c).LineWidth = 1;
+if bChildLineWidth
+    for c = 1:length(hax.Children)
+        if isprop(hax.Children(c),'LineWidth') && hax.Children(c).LineWidth < 1
+            hax.Children(c).LineWidth = 1;
+        end
     end
 end
