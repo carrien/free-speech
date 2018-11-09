d17376f21323957272e3da9178ec71093ac4d124
Carrie Niziolek <cniziolek@wisc.edu>
2018-11-08 18:03:05 -0600

    Argument structure (and cosmetic) changes

--- plotting/plot_rawFmtTracks.m
+++ plotting/plot_rawFmtTracks.m
@@ -1,57 +1,59 @@
-function [] = plot_rawFmtTracks(dataVals,taxis,grouping,trialset)
+function [] = plot_rawFmtTracks(dataVals,grouping,trialset)
 %PLOT_RAWFMTTRACKS  Plot formant tracks from dataVals object.
-%   PLOT_RAWFMTTRACKS(DATAVALS,TAXIS,SPLIT,TRIALSET) plots the first and
-%   second formant tracks from each trial in TRIALSET against an optional
-%   time axis TAXIS.  GROUPING defines the field in DATAVALS by which data
-%   should be grouped; e.g. GROUPING = 'vowel' will create a separate plot
-%   for each vowel.
+%   PLOT_RAWFMTTRACKS(DATAVALS,GROUPING,TRIALSET) plots the first and
+%   second formant tracks from each trial in TRIALSET.  GROUPING defines
+%   the field in DATAVALS by which data should be grouped; e.g. GROUPING =
+%   'vowel' will create a separate plot for each vowel.
 
-if nargin < 2 || isempty(taxis), taxis = 1; end
-if nargin < 3 || isempty(grouping), grouping = 'vowel'; end
-if nargin < 4, trialset = 1:length(dataVals); end
+if nargin < 1 || isempty(dataVals)
+    fprintf('Loading dataVals from current directory...')
+    load dataVals.mat;
+    fprintf(' done.\n')
+    if exist(fullfile(cd,'expt.mat'),'file')
+        fprintf('Loading expt from current directory...')
+        load expt.mat;
+        fprintf(' done.\n')
+        groupnames = expt.(sprintf('%ss',grouping));
+    end
+end
+if nargin < 2 || isempty(grouping), grouping = 'vowel'; end
+if nargin < 3, trialset = 1:length(dataVals); end
 
-tstep = .003;
+f1color = [0 0 1]; % blue
+f2color = [1 0 0]; % red
 
-for j=unique([dataVals.(grouping)])
+for g=unique([dataVals.(grouping)])
     figure;
+    % plot tracks
     for i=trialset
-        if (~isfield(dataVals,'bExcl') || ~dataVals(i).bExcl) && dataVals(i).(grouping) == j
-            len = length(dataVals(i).f1);
-            if taxis
-                plot(tstep.*(0:len-1),dataVals(i).f1,'b');
-                hold on;
-                plot(tstep.*(0:len-1),dataVals(i).f2,'r');
-            else
-                plot(dataVals(i).f1,'b');
-                hold on;
-                plot(dataVals(i).f2,'r');
-            end
+        if (~isfield(dataVals,'bExcl') || ~dataVals(i).bExcl) && dataVals(i).(grouping) == g
+            taxis = dataVals(i).ftrack_taxis - dataVals(i).ftrack_taxis(1);
+            plot(taxis,dataVals(i).f1,'Color',f1color);
+            hold on;
+            plot(taxis,dataVals(i).f2,'Color',f2color);
         end
     end
     
     % plot ends
     for i=trialset
-        if (~isfield(dataVals,'bExcl') || ~dataVals(i).bExcl) && dataVals(i).(grouping) == j
-            len = length(dataVals(i).f1);
-            if taxis
-                plot(tstep*(len-1),dataVals(i).f1(end),'co');
-                plot(tstep*(len-1),dataVals(i).f2(end),'mo');
-            else
-                plot(len,dataVals(i).f1(end),'co');
-                plot(len,dataVals(i).f2(end),'mo');
-            end
-            
+        if (~isfield(dataVals,'bExcl') || ~dataVals(i).bExcl) && dataVals(i).(grouping) == g
+            taxis = dataVals(i).ftrack_taxis - dataVals(i).ftrack_taxis(1);
+            x = taxis(end);
+            plot(x,dataVals(i).f1(end),'o','MarkerEdgeColor',f1color,'MarkerFaceColor',get_lightcolor(f1color,1.2),'MarkerSize',5);
+            plot(x,dataVals(i).f2(end),'o','MarkerEdgeColor',f2color,'MarkerFaceColor',get_lightcolor(f2color,1.2),'MarkerSize',5);
         end
     end
     
-    title(sprintf('%s %d',grouping,j))
-    if taxis
-        xlabel('time (s)')
+    % figure labels
+    if exist('groupnames','var')
+        titlesuffix = sprintf(': %s',groupnames{g});
     else
-        xlabel('sample number')
+        titlesuffix = [];
     end
+    title(sprintf('%s %d%s',grouping,g,titlesuffix))
+    xlabel('time (s)')
     ylabel('frequency (Hz)')
     box off;
-    makeFig4Screen;
+    makeFig4Screen([],0);
     
 end
