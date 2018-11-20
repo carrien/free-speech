4474a9c5f10768863327be6771f32d62c8ac592e
Ben Parrell <bparrell@wisc.edu>
2018-11-20 10:45:49 -0600

    edits to merge_fmtMatrices
    Changed merge_fmtMatrices to work with any number of conditions instead of only two.
    
    Currently there is an issue with getAcoustSubjPath because it calls a nonexistant function get_exptPath

--- speech/merge_fmtMatrices.m
+++ speech/merge_fmtMatrices.m
@@ -6,45 +6,52 @@ function [] = merge_fmtMatrices(exptName,snum,plotfile,conds2merge,mergednames)
 %   condition names (strings). MERGEDNAMES is an array of new condition
 %   names for each pair.
 
-dataPath = getAcoustSubjPath(exptName,snum);
+% dataPath = getAcoustSubjPath(exptName,snum); %BP note: this function is
+% currently broken as of 11/20/2018
+dataPath = pwd;
 load(fullfile(dataPath,plotfile),'fmtMatrix','fmtMeans','tstep','bMels','bFilt') % e.g. fmtTraces_3subj.mat
 analyses = fieldnames(fmtMeans); %#ok<NODEF>
 
 for c2m = 1:length(conds2merge)
-    oldcnd1 = conds2merge{c2m}{1};
-    oldcnd2 = conds2merge{c2m}{2};
+    nSubConds = length(conds2merge{c2m});
+    for iSubConds = 1:nSubConds
+        oldcnd{iSubConds} = conds2merge{c2m}{iSubConds};
+    end
+    
     newcnd = mergednames{c2m};
     
     for a = 1:length(analyses)
         anl = analyses{a};
         
         % fmtMatrix (all trials: ffx)
-        merge1 = fmtMatrix.(anl).(oldcnd1);
-        merge2 = fmtMatrix.(anl).(oldcnd2);
-        if size(merge1,1) == size(merge2,1)
-            fmtMatrix.(anl).(newcnd) = [merge1 merge2];
-        else
-            fmtMatrix.(anl).(newcnd) = merge1;
-            for i=1:size(merge2,2)
-                fmtMatrix.(anl).(newcnd) = nancat(fmtMatrix.(anl).(newcnd),merge2(:,i));
-            end
+        for iSubConds = 1:nSubConds
+            merge{iSubConds} = fmtMatrix.(anl).(oldcnd{iSubConds});
         end
-        fmtMatrix.(anl) = rmfield(fmtMatrix.(anl),oldcnd1);
-        fmtMatrix.(anl) = rmfield(fmtMatrix.(anl),oldcnd2);
+
+        fmtMatrix.(anl).(newcnd) = merge{1};
+        for iSubConds=2:nSubConds
+            fmtMatrix.(anl).(newcnd) = nancat(fmtMatrix.(anl).(newcnd),merge{iSubConds});
+        end
+    
+        for iSubConds=1:nSubConds
+            fmtMatrix.(anl) = rmfield(fmtMatrix.(anl),oldcnd{iSubConds});
+        end
         
         % fmtMeans (one average trial: rfx)
-        merge1 = fmtMeans.(anl).(oldcnd1);
-        merge2 = fmtMeans.(anl).(oldcnd2);
-        if size(merge1,1) == size(merge2,1)
-            fmtMeans.(anl).(newcnd) = [merge1 merge2];
-        else
-            fmtMeans.(anl).(newcnd) = merge1;
-            for i=1:size(merge2,2)
-                fmtMeans.(anl).(newcnd) = nancat(fmtMeans.(anl).(newcnd),merge2(:,i));
-            end
+        for iSubConds = 1:nSubConds
+            merge{iSubConds} = fmtMeans.(anl).(oldcnd{iSubConds});
         end
-        fmtMeans.(anl) = rmfield(fmtMeans.(anl),oldcnd1);
-        fmtMeans.(anl) = rmfield(fmtMeans.(anl),oldcnd2);
+
+        fmtMeans.(anl).(newcnd) = merge{1};
+        for iSubConds=2:nSubConds
+            fmtMeans.(anl).(newcnd) = nancat(fmtMeans.(anl).(newcnd),merge{iSubConds});
+        end
+        fmtMeans.(anl).(newcnd) = nanmean(fmtMeans.(anl).(newcnd),2);
+
+        for iSubConds=1:nSubConds
+            fmtMeans.(anl) = rmfield(fmtMeans.(anl),oldcnd{iSubConds});
+        end
+
     end
     
     % hasNpercent
