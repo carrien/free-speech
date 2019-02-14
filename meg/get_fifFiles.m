ebddab5c7cd964cf505e1a4ad617d9a595774da0
sbakst <bakst.sarah@gmail.com>
2019-02-14 15:02:24 -0600

    return list of .fif files from subject tsss folder

--- meg/get_fifFiles.m
+++ meg/get_fifFiles.m
@@ -1,8 +1,8 @@
-function [sfif] = get_FifFiles(exptName,sid)
+function [sfif] = get_fifFiles(exptName,sid)
 %GET_FIFFILES : get list of .fif files
 
-expDataPath = fullfile('/Volumes/smng/experiments/', exptName)
-fifdir = fullfile(expDataPath, 'megdata',sid, 'tsss')
+expDataPath = fullfile('/Volumes/smng/experiments/', exptName);
+fifdir = fullfile(expDataPath, 'megdata',sid, 'tsss');
 
 d = dir(fullfile(fifdir,'*.fif'));
 z = d(end); d(end) = []; d = [z; d]; % reorder to put unsuffixed file first
