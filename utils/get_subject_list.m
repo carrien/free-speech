8fcf875ab169214c8523407fe604391934a7348b
sbakst <bakst.sarah@gmail.com>
2019-02-27 11:26:40 -0600

    lists all non . or .. subjects in directory

---
+++ utils/get_subject_list.m
@@ -0,0 +1,15 @@
+function [subs] = get_subject_list(dataPath)
+
+% returns list of subjects in a directory, eg acousticdata
+
+if nargin < 1 || isempty(dataPath), dataPath = cd; end
+
+% LIA = ISMEMBER(A,B) for arrays A and B returns an array of the same
+%   size as A containing true where the elements of A are in B and false
+%   otherwise.
+
+
+subs = dirlist(dataPath);
+badnames = {'.','..'}
+folds2rm = ismember(names,badnames)
+subs(folds2rm) = [];
