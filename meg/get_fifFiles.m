571bbb36026f36f6cb879d0424766b09eefea906
sbakst <bakst.sarah@gmail.com>
2019-03-15 07:51:53 -0500

    exclude ER file

--- meg/get_fifFiles.m
+++ meg/get_fifFiles.m
@@ -4,6 +4,15 @@ function [sfif] = get_fifFiles(exptName,sid)
 expDataPath = fullfile('/Volumes/smng/experiments/', exptName);
 fifdir = fullfile(expDataPath, 'megdata',sid, 'tsss');
 
+rows2rm = [];
 d = dir(fullfile(fifdir,'*.fif'));
+for i = 1:length(d)
+    if strfind(d(i).name,'ER')
+        rows2rm = [rows2rm i]
+    end
+end
+
+d(rows2rm) = [];
+
 z = d(end); d(end) = []; d = [z; d]; % reorder to put unsuffixed file first
 sfif = fullfile(fifdir,{d.name});
