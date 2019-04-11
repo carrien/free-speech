213dd7bb2e761fd2e1e207676594ad1d7c3d0c71
sbakst <bakst.sarah@gmail.com>
2019-04-11 16:34:21 -0500

    words added for port

--- speech/txt2ipa.m
+++ speech/txt2ipa.m
@@ -9,17 +9,17 @@ end
 vowels = {'aa' 'ae' 'ah' 'ay' 'eh' 'ey' 'ih' 'iy' 'ow' 'uw' 'er' 'oe' 'uh'};
 
 aa = ismember(txtcell,{'aa' 'ah' 'a' 'sop' 'sock' 'sod' 'shop' 'shock' 'shot' 'shawl' 'saw'}); % added 'a' because this was the original vowel name for this category. Possibly will need to change it if we ever use the indefinite article.
-ae = ismember(txtcell,{'ae' 'add' 'ad' 'rad' 'yallow'});
+ae = ismember(txtcell,{'ae' 'add' 'ad' 'dad' 'rad' 'yallow'});
 ah = ismember(txtcell,{'above'});
 ay = ismember(txtcell,{'sigh' 'shy'});
-eh = ismember(txtcell,{'eh' 'E' 'Ed' 'bed' 'dead' 'head' 'ted' 'red' 'glen' 'blend' 'hais' 'eff' 'adept' 'meta' 'yellow'});
+eh = ismember(txtcell,{'eh' 'E' 'Ed' 'bed' 'dead' 'head' 'ted' 'red' 'glen' 'blend' 'hais' 'eff' 'fait' 'adept' 'meta' 'yellow' 'said'});
 ey = ismember(txtcell,{'eI' 'grain' 'beta' 'abate'});
 ih = ismember(txtcell,{'ih' 'I' 'rid' 'grin' 'yillow' 'pink' 'bink' 'sip' 'sin' 'shin' 'ship'});
-iy = ismember(txtcell,{'i' 'ee' 'eat' 'reed' 'green' 'bleed' 'creed' 'kreen' 'Yves' 'Eve' 'see' 'seep' 'seat' 'sea' 'sheep' 'sheet' 'she'});
+iy = ismember(txtcell,{'i' 'ee' 'eat' 'reed' 'green' 'bleed' 'creed' 'kreen' 'Yves' 'Eve' 'see' 'seep' 'seat' 'sea' 'sheep' 'sheet' 'she' 'vee' 'vie'});
 ow = ismember(txtcell,{'o' 'oh' 'blow' 'bode' 'so' 'sore' 'show' 'shore'});
 uw = ismember(txtcell,{'u' 'oo' 'rude' 'groom' 'blue' 'plume' 'plue' 'sue' 'suit' 'soon' 'shoe' 'shoot' 'shoes'});
 er = ismember(txtcell,{'er' 'blur'});
-oe = ismember(txtcell,{'oeuf'});
+oe = ismember(txtcell,{'ceux' 'oeuf'});
 uh = ismember(txtcell,{'good'});
         
 vowelinds = [aa; ae; ah; ay; eh; ey; ih; iy; ow; uw; er; oe; uh];
