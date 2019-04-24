528a173edf9c4632ad6d7b41908ff39fef8ef057
Carrie Niziolek <cniziolek@wisc.edu>
2019-04-24 13:48:01 -0500

    Add vowels to txt2ipa map

--- speech/txt2ipa.m
+++ speech/txt2ipa.m
@@ -10,17 +10,17 @@ vowels = {'aa' 'ae' 'ah' 'ay' 'eh' 'ey' 'ih' 'iy' 'ow' 'uw' 'er' 'oe' 'uh'};
 
 aa = ismember(txtcell,{'aa' 'ah' 'a' 'sop' 'sock' 'sod' 'shop' 'shock' 'shot' 'shawl' 'saw'}); % added 'a' because this was the original vowel name for this category. Possibly will need to change it if we ever use the indefinite article.
 ae = ismember(txtcell,{'ae' 'add' 'ad' 'rad' 'yallow'});
-ah = ismember(txtcell,{'above'});
-ay = ismember(txtcell,{'sigh' 'shy'});
+ah = ismember(txtcell,{'ah' 'above'});
+ay = ismember(txtcell,{'ay' 'sigh' 'shy'});
 eh = ismember(txtcell,{'eh' 'E' 'Ed' 'bed' 'dead' 'head' 'ted' 'red' 'glen' 'blend' 'hais' 'eff' 'adept' 'meta' 'yellow'});
-ey = ismember(txtcell,{'eI' 'grain' 'beta' 'abate'});
+ey = ismember(txtcell,{'ey' 'eI' 'grain' 'beta' 'abate'});
 ih = ismember(txtcell,{'ih' 'I' 'rid' 'grin' 'yillow' 'pink' 'bink' 'sip' 'sin' 'shin' 'ship'});
-iy = ismember(txtcell,{'i' 'ee' 'eat' 'reed' 'green' 'bleed' 'creed' 'kreen' 'Yves' 'Eve' 'see' 'seep' 'seat' 'sea' 'sheep' 'sheet' 'she'});
-ow = ismember(txtcell,{'o' 'oh' 'blow' 'bode' 'so' 'sore' 'show' 'shore'});
-uw = ismember(txtcell,{'u' 'oo' 'rude' 'groom' 'blue' 'plume' 'plue' 'sue' 'suit' 'soon' 'shoe' 'shoot' 'shoes'});
+iy = ismember(txtcell,{'iy' 'i' 'ee' 'eat' 'reed' 'green' 'bleed' 'creed' 'kreen' 'Yves' 'Eve' 'see' 'seep' 'seat' 'sea' 'sheep' 'sheet' 'she'});
+ow = ismember(txtcell,{'ow' 'o' 'oh' 'blow' 'bode' 'so' 'sore' 'show' 'shore'});
+uw = ismember(txtcell,{'uw' 'u' 'oo' 'rude' 'groom' 'blue' 'plume' 'plue' 'sue' 'suit' 'soon' 'shoe' 'shoot' 'shoes'});
 er = ismember(txtcell,{'er' 'blur'});
-oe = ismember(txtcell,{'oeuf'});
-uh = ismember(txtcell,{'good'});
+oe = ismember(txtcell,{'oe' 'oeuf'});
+uh = ismember(txtcell,{'uh' 'good'});
         
 vowelinds = [aa; ae; ah; ay; eh; ey; ih; iy; ow; uw; er; oe; uh];
 vowelinds = sum(vowelinds .* repmat([1:size(vowelinds,1)]',1,size(vowelinds,2)),1);
