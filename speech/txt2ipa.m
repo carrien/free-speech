bf4b1c2cd347b5a1a64fb507369fe1437d0275ff
Carrie Niziolek <cniziolek@wisc.edu>
2019-06-27 19:04:51 -0500

    Adding accumulated changes

--- speech/txt2ipa.m
+++ speech/txt2ipa.m
@@ -6,23 +6,24 @@ elseif ischar(txt), txtcell = {txt}; % if string, convert to cell array
 else, error('Input must be a text string or cell array of strings.')
 end
 
-vowels = {'aa' 'ae' 'ah' 'ay' 'eh' 'ey' 'ih' 'iy' 'ow' 'uw' 'er' 'oe' 'uh'};
+vowels = {'aa' 'ae' 'ah' 'ay' 'eh' 'ey' 'ih' 'iy' 'ow' 'uw' 'er' 'oe' 'ou' 'uh'};
 
 aa = ismember(txtcell,{'aa' 'ah' 'a' 'bod' 'sop' 'sock' 'sod' 'shop' 'shock' 'shot' 'shawl' 'saw'}); % added 'a' because this was the original vowel name for this category. Possibly will need to change it if we ever use the indefinite article.
-ae = ismember(txtcell,{'ae' 'add' 'ad' 'bad' 'dad' 'rad' 'sad' 'yallow'});
+ae = ismember(txtcell,{'ae' 'add' 'ad' 'bad' 'bat' 'dad' 'pat' 'rad' 'sad' 'sat' 'yallow'});
 ah = ismember(txtcell,{'ah' 'above'});
 ay = ismember(txtcell,{'ay' 'sigh' 'shy'});
-eh = ismember(txtcell,{'eh' 'E' 'Ed' 'bed' 'dead' 'fed' 'head' 'ted' 'red' 'glen' 'blend' 'hais' 'eff' 'fait' 'adept' 'meta' 'yellow' 'said'});
+eh = ismember(txtcell,{'eh' 'E' 'Ed' 'bed' 'bet' 'blend' 'dead' 'fed' 'head' 'ted' 'red' 'glen'  'hais' 'eff' 'fait' 'adept' 'meta' 'pet' 'set' 'said' 'yellow'});
 ey = ismember(txtcell,{'ey' 'eI' 'grain' 'beta' 'abate'});
-ih = ismember(txtcell,{'ih' 'I' 'rid' 'grin' 'yillow' 'pink' 'bink' 'sip' 'sin' 'shin' 'ship'});
+ih = ismember(txtcell,{'ih' 'I' 'bit' 'bink' 'rid' 'grin' 'yillow' 'pink' 'pit' 'sip' 'sin' 'sit' 'shin' 'ship'});
 iy = ismember(txtcell,{'iy' 'i' 'bead' 'ee' 'eat' 'reed' 'green' 'bleed' 'creed' 'kreen' 'Yves' 'Eve' 'see' 'seep' 'seat' 'sea' 'sheep' 'sheet' 'she' 'vee' 'vie'});
 ow = ismember(txtcell,{'ow' 'o' 'oh' 'blow' 'bode' 'so' 'sore' 'show' 'shore'});
 uw = ismember(txtcell,{'uw' 'u' 'booed' 'oo' 'rude' 'groom' 'blue' 'plume' 'plue' 'sue' 'suit' 'soon' 'shoe' 'shoot' 'shoes'});
 er = ismember(txtcell,{'er' 'blur'});
-oe = ismember(txtcell,{'oe' 'ceux' 'oeuf'});
+oe = ismember(txtcell,{'oe' 'oeuf' 'neuf'});
+ou = ismember(txtcell,{'ceux'});
 uh = ismember(txtcell,{'uh' 'good'});
 
-vowelinds = [aa; ae; ah; ay; eh; ey; ih; iy; ow; uw; er; oe; uh];
+vowelinds = [aa; ae; ah; ay; eh; ey; ih; iy; ow; uw; er; oe; ou; uh];
 vowelinds = sum(vowelinds .* repmat([1:size(vowelinds,1)]',1,size(vowelinds,2)),1);
 if any(~vowelinds)
     notfound = unique(txtcell(~vowelinds));
