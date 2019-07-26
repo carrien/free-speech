0e38af5a7d727e594f1ab78e39ed62851041993d
Ben Parrell <bparrell@wisc.edu>
2019-07-26 13:00:38 -0500

    changes for voystick

--- speech/txt2ipa.m
+++ speech/txt2ipa.m
@@ -6,15 +6,15 @@ elseif ischar(txt), txtcell = {txt}; % if string, convert to cell array
 else, error('Input must be a text string or cell array of strings.')
 end
 
-vowels = {'aa' 'ae' 'ah' 'ay' 'eh' 'ey' 'ih' 'iy' 'ow' 'uw' 'er' 'oe' 'ou' 'uh' 'xx'};
+vowels = {'aa' 'ae' 'ah' 'ay' 'eh' 'ey' 'ih' 'iy' 'ow' 'uw' 'er' 'oe' 'ou' 'uh' 'uu' 'xx'};
 
 aa = ismember(txtcell,{'aa' 'ah' 'a' 'bod' 'sop' 'sock' 'sod' 'shop' 'shock' 'shot' 'shawl' 'saw'}); % added 'a' because this was the original vowel name for this category. Possibly will need to change it if we ever use the indefinite article.
 ae = ismember(txtcell,{'ae' 'add' 'ad' 'bad' 'bat' 'dad' 'pat' 'rad' 'sad' 'sat' 'yallow'});
-ah = ismember(txtcell,{'ah' 'above'});
+ah = ismember(txtcell,{'ah' 'above' 'bud'});
 ay = ismember(txtcell,{'ay' 'sigh' 'shy'});
 eh = ismember(txtcell,{'eh' 'E' 'Ed' 'bed' 'bet' 'blend' 'dead' 'fed' 'head' 'ted' 'red' 'glen'  'hais' 'eff' 'fait' 'adept' 'meta' 'pet' 'set' 'said' 'yellow'});
 ey = ismember(txtcell,{'ey' 'eI' 'grain' 'beta' 'abate'});
-ih = ismember(txtcell,{'ih' 'I' 'bit' 'bink' 'rid' 'grin' 'yillow' 'pink' 'pit' 'sip' 'sin' 'sit' 'shin' 'ship'});
+ih = ismember(txtcell,{'ih' 'I' 'bit' 'bid' 'bink' 'rid' 'grin' 'yillow' 'pink' 'pit' 'sip' 'sin' 'sit' 'shin' 'ship'});
 iy = ismember(txtcell,{'iy' 'i' 'bead' 'ee' 'eat' 'reed' 'green' 'bleed' 'creed' 'kreen' 'Yves' 'Eve' 'see' 'seep' 'seat' 'sea' 'sheep' 'sheet' 'she' 'vee' 'vie'});
 ow = ismember(txtcell,{'ow' 'o' 'oh' 'blow' 'bode' 'so' 'sore' 'show' 'shore'});
 uw = ismember(txtcell,{'uw' 'u' 'booed' 'oo' 'rude' 'groom' 'blue' 'plume' 'plue' 'sue' 'suit' 'soon' 'shoe' 'shoot' 'shoes'});
@@ -22,9 +22,10 @@ er = ismember(txtcell,{'er' 'blur'});
 oe = ismember(txtcell,{'oe' 'oeuf' 'neuf'});
 ou = ismember(txtcell,{'ceux'});
 uh = ismember(txtcell,{'uh' 'good'});
+uu = ismember(txtcell,{'uu'});
 xx = ismember(txtcell,{'xx' '*' '**' '***'});
 
-vowelinds = [aa; ae; ah; ay; eh; ey; ih; iy; ow; uw; er; oe; ou; uh; xx];
+vowelinds = [aa; ae; ah; ay; eh; ey; ih; iy; ow; uw; er; oe; ou; uh; uu; xx];
 vowelinds = sum(vowelinds .* repmat([1:size(vowelinds,1)]',1,size(vowelinds,2)),1);
 if any(~vowelinds)
     notfound = unique(txtcell(~vowelinds));
