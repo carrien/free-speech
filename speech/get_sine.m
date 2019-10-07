035768e2e3af8544d15e30f162e499f860c78890
Carrie Niziolek <cniziolek@wisc.edu>
2019-10-07 17:17:01 -0500

    Updates to acoustics plotting/sound f'ns

--- speech/get_sine.m
+++ speech/get_sine.m
@@ -1,14 +1,15 @@
-function [sinewave,t] = get_sine(freq,dur,fs,phaseoffset)
-%GET_SINE  Create sine wave of specified frequency, duration, sampling, and phase offset.
-%  GET_SINE(FREQ,DUR,FS,PHASEOFFSET) returns a sine wave SINEWAVE with
-%  frequency FREQ (Hz), duration DUR (s), sampling rate FS (samples/s), and
-%  phase offset PHASEOFFSET (radians).
+function [sinewave,t] = get_sine(freq,amp,phaseoffset,dur,fs)
+%GET_SINE  Create sine wave.
+%  GET_SINE(FREQ,AMP,PHASEOFFSET,DUR,FS) returns a sine wave SINEWAVE with
+%  frequency FREQ (Hz), amplitude AMP, phase offset PHASEOFFSET (radians),
+%  duration DUR (s), and sampling rate FS (samples/s).
 
-if nargin < 2 || isempty(dur), dur = 1; end
-if nargin < 3 || isempty(fs), fs = 11025; end
-if nargin < 4 || isempty(phaseoffset), phaseoffset = 0; end
+if nargin < 2 || isempty(amp), amp = 1; end
+if nargin < 3 || isempty(phaseoffset), phaseoffset = 0; end
+if nargin < 4 || isempty(dur), dur = 1; end
+if nargin < 5 || isempty(fs), fs = 11025; end
 
-t = 0:1/fs:dur;
-sinewave = sin(freq*2*pi*t + phaseoffset);
+t = 1/fs:1/fs:dur;
+sinewave = amp*sin(freq*2*pi*t + phaseoffset);
 
 end
