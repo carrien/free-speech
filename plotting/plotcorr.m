c9f7bb6568de0207414956e0720226c07919f7f7
Carrie Niziolek <cniziolek@wisc.edu>
2019-10-16 10:36:15 -0500

    Aesthetic updates to plotcorr

--- plotting/plotcorr.m
+++ plotting/plotcorr.m
@@ -1,10 +1,16 @@
-function [r,p,pfit] = plotcorr(x,y,loc,tail,corrtype)
+function [r,p,pfit] = plotcorr(x,y,style)
 %PLOTCORR  Plot the correlation and best fit line for two vectors.
 %   PLOTCORR(X,Y,LOC,TAIL,CORRTYPE)
 
-if nargin < 3 || isempty(loc), loc = 'Best'; end
-if nargin < 4 || isempty(tail), tail = 'both'; end
-if nargin < 4 || isempty(corrtype), corrtype = 'Pearson'; end
+%if nargin < 3 || isempty(loc), loc = 'Best'; end
+%if nargin < 4 || isempty(tail), tail = 'both'; end
+%if nargin < 4 || isempty(corrtype), corrtype = 'Pearson'; end
+if nargin < 3 || isempty(style), style = 'k.'; end
+
+%loc = 'Best';
+loc = 'SouthEast';
+tail = 'both';
+corrtype = 'Pearson';
 
 if size(x,1)==1, x = x'; end
 if size(y,1)==1, y = y'; end
@@ -25,12 +31,12 @@ if sum(isnan(x)) || sum(isnan(y)) %remove NaNs
 end
 
 [r,p] = corr(x,y,'type',corrtype,'tail',tail);
-plot(x,y,'k.')
+plot(x,y,style,'MarkerSize',15)
 pfit = polyfit(x,y,1);
 hold on;
 %axis equal
 ax = axis; xrange = ax(1)-5:ax(2)+5;
-plot(xrange,xrange*pfit(1)+pfit(2),'k'); axis(ax);
+plot(xrange,xrange*pfit(1)+pfit(2),style(1)); axis(ax);
 legend({sprintf('r = %.3f',r),sprintf('p = %.3f',p)},'Location',loc)
 
 xlab = inputname(1);
