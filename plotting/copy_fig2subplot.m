0b303907ef97c60d2072c2fd6bb4a2b68a2afbe6
Carrie Niziolek <cniziolek@wisc.edu>
2019-11-14 20:49:52 -0600

    Add input arg defaults

--- plotting/copy_fig2subplot.m
+++ plotting/copy_fig2subplot.m
@@ -1,6 +1,11 @@
-function [ ] = copy_fig2subplot(hfig2copy,hnewfig,nrows,ncols,subplotnum)
+function [HNEWFIG] = copy_fig2subplot(hfig2copy,hnewfig,nrows,ncols,subplotnum)
 %COPY_FIG2SUBPLOT  Copy a single-axes figure to a subplot in a new figure.
-%   COPY_FIG2SUBPLOT(HFIG
+%   COPY_FIG2SUBPLOT(HFIG2COPY,HNEWFIG)
+
+if nargin < 2 || isempty(hnewfig), hnewfig = figure; end
+if nargin < 3 || isempty(nrows), nrows = 1; end
+if nargin < 4 || isempty(ncols), ncols = 2; end
+if nargin < 5 || isempty(subplotnum), subplotnum = 1; end
 
 % get handle to axis
 hax = hfig2copy.Children; % assume only one axis
