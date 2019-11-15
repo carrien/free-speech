3cf4dd241dcafd790b60b7d4f849caa40fb87f24
Carrie Niziolek <cniziolek@wisc.edu>
2019-11-14 20:59:09 -0600

    Add user-defined input params

--- plotting/makeFig4Screen.m
+++ plotting/makeFig4Screen.m
@@ -1,15 +1,24 @@
-function [] = makeFig4Screen(hax,bChildLineWidth)
+function [] = makeFig4Screen(hax,p,bChildLineWidth)
 
 if nargin < 1 || isempty(hax), hax = gca; end
-if nargin < 2 || isempty(bChildLineWidth), bChildLineWidth = 1; end
+if nargin < 2 || isempty(p), p = struct; end
+if nargin < 3 || isempty(bChildLineWidth), bChildLineWidth = 1; end
 
+% defaults
 hax.LineWidth = 1;
 hax.XColor = [0 0 0];
 hax.YColor = [0 0 0];
-hax.FontName = 'Arial';
+hax.FontName = 'Helvetica';
 hax.FontSize = 14;
-%set(gcf,'Color','none')
 box off;
+%set(gcf,'Color','none')
+
+% user-defined params
+fields = fieldnames(p);
+for fn = 1:length(fields)
+    fieldname = fields{fn};
+    hax.(fieldname) = p.(fieldname);
+end
 
 if bChildLineWidth
     for c = 1:length(hax.Children)
