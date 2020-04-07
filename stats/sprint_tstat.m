655ac0144005cd40a14f1ff6cc61432d26571526
Carrie Niziolek <cniziolek@wisc.edu>
2020-04-07 16:34:04 -0500

    Add pmin option to sprint_tstat

--- stats/sprint_tstat.m
+++ stats/sprint_tstat.m
@@ -1,8 +1,17 @@
-function [str] = sprint_tstat(p,stats)
+function [str] = sprint_tstat(p,stats,pmin)
 %SPRINT_TSTAT  Print t-statistic in formatted string.
 %   SPRINT_TSTAT(P,STATS)
 
-str = sprintf('t(%d) = %f, p = %f',stats.df,stats.tstat,p);
+if nargin < 3, pmin = 0; end
+
+tstr = sprintf('t(%d) = %.4f',stats.df,stats.tstat);
+if p < pmin
+    pstr = sprintf('p < %.4f',pmin);
+else
+    pstr = sprintf('p = %.4f',p);
+end
+
+str = sprintf('%s, %s',tstr,pstr);
 
 end
 
