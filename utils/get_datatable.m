d916d3234d6db964cde97f4ad9bcc35efd8be6ee
Carrie Niziolek <cniziolek@wisc.edu>
2020-04-07 21:17:31 -0500

    Support more classes in table conversion

--- utils/get_datatable.m
+++ utils/get_datatable.m
@@ -19,9 +19,9 @@ nfactors = length(factors);
 for f = 1:nfactors
     factname = factors{f};
     factval = fact.(factname);
-    if isnumeric(factval) % if a number
+    if isnumeric(factval) || iscell(factval) || iscategorical(factval) || islogical(factval) || isstring(factval)
         if length(factval)==1
-            data.(factname) = factval*ones(nObs,1);
+            data.(factname) = repmat(factval,nObs,1);
         elseif iscolumn(factval) && length(factval)==nObs
             data.(factname) = factval;
         elseif isrow(factval) && length(factval)==nObs
@@ -30,21 +30,9 @@ for f = 1:nfactors
             error('Length of factor is %d. Factor must either be a single element or have the same number of elements as the data to be added (length: %d).',length(factval),nObs);
         end
     elseif ischar(factval)
-        C = cell(nObs,1);
-        C(:) = {factval};
-        data.(factname) = C;
-    elseif iscell(factval)
-        if length(factval)==1
-            C = cell(nObs,1);
-            C(:) = factval;
-            data.(factname) = C;
-        elseif iscolumn(factval) && length(factval)==nObs
-            data.(factname) = factval;
-        elseif isrow(factval) && length(factval)==nObs
-            data.(factname) = factval';
-        else
-            error('Length of factor is %d. Factor must either be a single element or have the same number of elements as the data to be added (length: %d).',length(factval),nObs);
-        end
+        data.(factname) = repmat({factval},nObs,1);
+    else
+        warning('Unknown class %s. Omitting from table.',class(factval))
     end
 end
 
