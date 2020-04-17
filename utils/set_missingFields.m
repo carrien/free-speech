292586bd74d6338a00253266edc2d0c633097447
Carrie Niziolek <cniziolek@wisc.edu>
2020-04-16 20:16:54 -0500

    Function to set fields from a default struct

---
+++ utils/set_missingFields.m
@@ -0,0 +1,19 @@
+function [s] = set_missingFields(s,defaults,bPrint)
+%SET_MISSINGFIELDS  Set fieldnames in structure if they are not defined.
+%   S = SET_MISSINGFIELDS(S,DEFAULTS) copies the fields from the struct
+%   array DEFAULTS to the struct array S unless they already exist in S.
+
+if isempty(s), s = struct; end
+if nargin < 3 || isempty(bPrint), bPrint = 1; end
+
+snames = fieldnames(s);
+dnames = fieldnames(defaults);
+missingIdx = find(~ismember(dnames,snames));
+
+for fn = 1:length(missingIdx)
+    fieldname = dnames{missingIdx(fn)};
+    s.(fieldname) = defaults.(fieldname);
+    if bPrint
+        fprintf('Setting default %s\n',fieldname)
+    end
+end
