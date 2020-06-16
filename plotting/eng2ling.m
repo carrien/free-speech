4ad3c484e47cf13ac69d134152d72f66888e5d32
Carrie Niziolek <cniziolek@wisc.edu>
2020-06-16 18:18:41 -0500

    Convert between vowel space conventions

---
+++ plotting/eng2ling.m
@@ -0,0 +1,25 @@
+function [ ] = eng2ling(ax)
+%ENG2LING  Converts between "engineering" and "linguistics" vowel spaces.
+%   ENG2LING(AX)
+
+% reverse x-axis
+if strcmp(ax.XDir,'normal')
+    ax.XDir = 'reverse';
+else
+    ax.XDir = 'normal';
+end
+
+% reverse y-axis
+if strcmp(ax.YDir,'normal')
+    ax.YDir = 'reverse';
+else
+    ax.YDir = 'normal';
+end
+
+% flip x and y axes
+[az,el] = view;
+if az==0 && el==90
+    view(ax,90,-90);
+elseif az==90 && el==-90
+    view(ax,0,90);
+end
