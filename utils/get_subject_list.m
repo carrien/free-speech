38ded32cbdc4b62fb4575f18fc7c853ac4e10a58
sbakst <bakst.sarah@gmail.com>
2020-07-01 16:34:33 -0500

    optionally exclude particular subjects

--- utils/get_subject_list.m
+++ utils/get_subject_list.m
@@ -1,8 +1,10 @@
-function [subs] = get_subject_list(dataPath)
+function [subs] = get_subject_list(dataPath,subs2exclude)
 
 % returns list of subjects in a directory, eg acousticdata
+% subs2exclude is a cell array in that directory.
 
 if nargin < 1 || isempty(dataPath), dataPath = cd; end
+if nargin < 2 || isempty(subs2exclude), subs2exclude=[]; end
 
 % LIA = ISMEMBER(A,B) for arrays A and B returns an array of the same
 %   size as A containing true where the elements of A are in B and false
@@ -15,3 +17,17 @@ badnames = {'.','..'};
 subs = {subs.name};
 folds2rm = ismember(subs,badnames);
 subs(folds2rm) = [];
+
+if isempty(subs2exclude)
+    subs2exclude='subs2exclude.mat';
+else
+    subs2exclude=[subs2exclude '.mat'];
+end
+
+
+if exist(subs2exclude) == 2
+    load(subs2exclude);
+    subs2rm = ismember(subs,subs2exclude);
+    subs(subs2rm)=[];
+end
+    
