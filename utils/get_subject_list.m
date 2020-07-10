1e5eb6806c461f465f032b6ffa65eb6ddbd7515a
sbakst <bakst.sarah@gmail.com>
2020-07-10 16:56:53 -0500

    boolean for sp IDs only

--- utils/get_subject_list.m
+++ utils/get_subject_list.m
@@ -1,10 +1,11 @@
-function [subs] = get_subject_list(dataPath,subs2exclude)
+function [subs] = get_subject_list(dataPath,subs2exclude,bSPonly)
 
 % returns list of subjects in a directory, eg acousticdata
 % subs2exclude is a cell array in that directory.
 
 if nargin < 1 || isempty(dataPath), dataPath = cd; end
 if nargin < 2 || isempty(subs2exclude), subs2exclude=[]; end
+if nargin < 3 || isempty(bSPonly), bSPonly=1; end
 
 % LIA = ISMEMBER(A,B) for arrays A and B returns an array of the same
 %   size as A containing true where the elements of A are in B and false
@@ -30,4 +31,9 @@ if exist(subs2exclude) == 2
     subs2rm = ismember(subs,subs2exclude);
     subs(subs2rm)=[];
 end
+
+if bSPonly
+    subs = subs(contains(subs,'sp'));
+end
+    
     
