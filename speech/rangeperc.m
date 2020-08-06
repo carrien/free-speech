c30e82c11b9e22a33865b60812d4baf3c2ac0ce6
Comeani <Ncomeau@wisc.edu>
2020-08-06 08:52:43 -0500

    function for getting the samples from an input signal that fall within a provided range

---
+++ speech/rangeperc.m
@@ -0,0 +1,24 @@
+function [rangeSig] = rangeperc(signal,range)
+%RANGEPERC   Returns the samples of input signal that fall within a
+%provided percentage range.
+%Range should be a vector [start end] containing the percentages
+% eg. the whole signal is [0 100]
+
+startPerc = range(1);
+endPerc = range(2);
+
+if startPerc < 0 || endPerc > 100
+    error('Supplied percentages must be between 0 and 100')
+elseif endPerc < startPerc
+    error('Second percentage supplied must be greater than the first')
+end
+
+startInd = ceil(startPerc*.01*length(signal));
+endInd = ceil(endPerc*.01*length(signal));
+
+
+rangeSig = signal(startInd:endInd);
+
+
+
+end
