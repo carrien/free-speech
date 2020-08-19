4db8f12e19cc8cba17965ac62d303f022eb249c3
Chris Naber <cwnaber@wisc.edu>
2020-08-19 11:50:35 -0500

    Added compatibility for passing deviceNumber through to KbWait call

--- utils/KbWaitForKey.m
+++ utils/KbWaitForKey.m
@@ -1,4 +1,4 @@
-function [keyCodeLogical] = KbWaitForKey(keys2wait4,timelimit)
+function [keyCodeLogical] = KbWaitForKey(keys2wait4, timelimit, deviceNumber)
 %KBWAITFORKEY  Wait for specific keypress.
 %   KBWAITFORKEY(KEY2WAIT4,TIMELIMIT) waits until one of the keys defined
 %   in KEYS2WAIT4 is pressed before continuing. If TIMELIMIT is defined,
@@ -7,12 +7,13 @@ function [keyCodeLogical] = KbWaitForKey(keys2wait4,timelimit)
 
 if nargin < 1 || isempty(keys2wait4), keys2wait4 = [32 49 50 51 52]; end % spacebar, 1, 2, 3, 4
 if nargin < 2 || isempty(timelimit), timelimit = inf; end
+if nargin < 3 || isempty(deviceNumber), deviceNumber = []; end
 
 tic;
 
 wait = 1;
 while wait && toc < timelimit
-    [~,keyCodeLogical] = KbWait;
+    [~,keyCodeLogical] = KbWait(deviceNumber);
     keyCode = find(keyCodeLogical);
     if any(keys2wait4 == keyCode(1))
         wait = 0;
