b91842d358ae95e15cc0f803d80a1b4b4b251e4b
Comeani <Ncomeau@wisc.edu>
2021-02-04 16:48:24 -0600

    Conditional for handling cloning with multiple figures

--- experiment_helpers/delete_exptText.m
+++ experiment_helpers/delete_exptText.m
@@ -2,4 +2,7 @@ function [] = delete_exptText(h_fig,h_2delete)
 
 delete(h_2delete);
 get_figinds_audapter;
-CloneFig(h_fig(stim),h_fig(dup))
+
+if length(h_fig) >= dup
+    CloneFig(h_fig(stim),h_fig(dup))
+end
