2dbcd11395a04ad059f8686c386bf134f88661bf
Chris Naber <cwnaber@wisc.edu>
2021-03-15 14:13:57 -0500

    updated header. removed test code.

--- experiment_helpers/validate_onsetOffset.m
+++ experiment_helpers/validate_onsetOffset.m
@@ -3,11 +3,16 @@ function [onset_bad, offset_bad] = validate_onsetOffset(dataPath, badThresh)
 %   relative to trial onset and offset. Used to validate that we're capturing
 %   the full utterance.
 %
+%   Returns up to two arguments. Each is a vector of trial numbers which
+%   are within `badThresh` seconds of the edge of the recording. The first
+%   is when the onset is too close to the beginning; the second is when the
+%   offset is too close to the end.
+%
 % 2021-03 CWN initial commit
 
 % input argument validation
 if nargin < 1 || isempty(dataPath), dataPath = pwd; end
-if nargin < 2 || isempty(badThresh), badThresh = 0.1; end
+if nargin < 2 || isempty(badThresh), badThresh = 0.1; end %default 100 ms
 
 
 load(fullfile(dataPath, 'data.mat'), 'data');
@@ -18,9 +23,6 @@ frameDur = data(1).params.frameLen / data(1).params.sRate;
 %preallocate
 onset  = zeros(1, length(data));
 offset = zeros(1, length(data));
-
-% for testing purposes
-data(1).fmts = zeros(985, 4);
 
 % find onsets and offsets
 for i = 1:length(data)
@@ -46,8 +48,8 @@ histogram(offset, 'BinWidth', 0.05);
 title('Offset distance to end');
 
 % collect trials too close to the edge
-badThresh_onset  = badThresh;   %default 100 ms
-badThresh_offset = badThresh;   %default 100 ms
+badThresh_onset  = badThresh;   
+badThresh_offset = badThresh;   
 
 onset_bad = find(onset <= badThresh_onset);
 offset_bad = find(offset <= badThresh_offset);
