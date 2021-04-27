57cc2ba65506d298bde5597ccc173535a49d0bf6
Robin Karlin <rkarlin@wisc.edu>
2021-04-26 20:06:30 -0500

    major change to check segment to take multiple events and call modularized get duration script

--- experiment_helpers/check_segmentDuration.m
+++ experiment_helpers/check_segmentDuration.m
@@ -1,4 +1,4 @@
-function expt = check_segmentDuration(expt, startStatus, endStatus, uevent1name, uevent2name)
+function expt = check_segmentDuration(expt, data, statuses, ueventNames, durationNames)
 % Find the duration of segments (or parts of segments), based on OST status changes 
 % Inputs: 
 % 1. expt: the expt that you are currently working with. Should provide information about tracking file
@@ -9,6 +9,7 @@ function expt = check_segmentDuration(expt, startStatus, endStatus, uevent1name,
 % --- defaults to startStatus + 2
 % 4. uevent1name (added 2021/02/02): optional user event name for the startStatus
 % 5. uevent2name (added 2021/02/02); optional user event name for the endStatus
+
 % 
 % Major change: RK 2021/02/02
 % - Now allows flexibility with WHICH statuses you want to use, using two input arguments: 
@@ -16,97 +17,95 @@ function expt = check_segmentDuration(expt, startStatus, endStatus, uevent1name,
 % --- endStatus: the end of the interval of interest (given as an OST status number)
 
 %%
-% if nargin < 1 || isempty(dataPath), dataPath = cd; end
-dataPath = fullfile(expt.dataPath,expt.listWords{1},'pre'); % This should probably be made better. 
+if strcmp(expt.name, 'timeAdapt')
+    dataPath = fullfile(expt.dataPath,expt.listWords{1},'pre'); % This should probably be made better. 
+else
+    dataPath = expt.dataPath; 
+end
+
+if nargin < 2 || isempty(data)
+    fprintf('Loading trial data... ');
+    load(fullfile(dataPath,'data.mat'))
+    fprintf('Done\n');
+end
+
+if nargin < 3 || isempty(statuses)
+    startStatus = get_pcf(expt.trackingFileLoc, expt.trackingFileName, 'time', '1', 'ostStat_initial'); % Get the triggering status
+    endStatus = startStatus + 2; 
+    statuses = [startStatus endStatus]; 
+end
+
+nStatusPairs = length(statuses) - 1; 
+if nStatusPairs < 1
+    warning('Attempting to get a duration with only one endpoint. Stopping script'); 
+    return
+end
+
+% Names for the audiogui events
+if nargin < 4 || isempty(ueventNames)
+    for i = 1:length(statuses)
+        ueventNames{i} = ['status' statuses(i)]; 
+    end
+end
+
+if nargin < 5 || isempty(durationNames)
+    for i = 1:nStatusPairs
+        durationNames{i} = ['duration_ost' num2str(statuses(i)) '_' num2str(statuses(i+1))]; 
+    end
+end
+
+%% Tracking file information
+
+if isfield(expt, 'trackingFileLoc')
+    trackingFileDir = expt.trackingFileLoc; 
+elseif isfield(expt, 'trackingFileDir')
+    trackingFileDir = expt.trackingFileDir; 
+end 
 
 % Get the word
 stimWord = expt.listWords{1}; 
 % Get the target OST values
-if strcmp(stimWord,'god') || strcmp(stimWord,'gar')
-    expt.trackingFileName = 'ada';   
+if isfield(expt, 'trackingFileName')
+    trackingFileName = expt.trackingFileName; 
+elseif strcmp(stimWord,'god') || strcmp(stimWord,'gar')
+    trackingFileName = 'ada';   
+    expt.trackingFileName = trackingFileName;  
 elseif strcmp(stimWord,'cod') || strcmp(stimWord,'car')    
-    expt.trackingFileName = 'ata';    
+    trackingFileName = 'ata';    
+    expt.trackingFileName = trackingFileName;
 elseif strcmp(stimWord,'sea') || strcmp(stimWord,'C') || strcmp(stimWord,'saw')
-    expt.trackingFileName = 'asa';     
+    trackingFileName = 'asa';     
+    expt.trackingFileName = trackingFileName;  
 elseif strcmp(stimWord,'czar') || strcmp(stimWord,'Z')
-    expt.trackingFileName = 'aza';     
+    trackingFileName = 'aza';     
+    expt.trackingFileName = trackingFileName;  
 elseif contains(stimWord,'apper')
-    expt.trackingFileName = stimWord; 
+    trackingFileName = stimWord; 
+    expt.trackingFileName = trackingFileName; 
 end
 
-% If startStatus empty, get the triggering one
-if nargin < 2 || isempty(startStatus)
-    startStatus = get_pcf(expt.trackingFileLoc, expt.trackingFileName, 'time', '1', 'ostStat_initial'); % Get the triggering status
+% If a working copy doesn't exist, make one
+ostWorking = fullfile(get_gitPath, trackingFileDir, [trackingFileName 'Working.ost']); 
+if exist(ostWorking,'file') ~= 2
+    refreshWorkingCopy(trackingFileDir, trackingFileName, 'ost');
 end
-% If endStatus empty, get the one immediately after startStatus
-if nargin < 3 || isempty(endStatus)
-    if strcmp(expt.trackingFileName,'ata') || strcmp(expt.trackingFileName,'capper')
-        ostAdd = 4;
-    else
-        ostAdd = 2; 
-    end
-    endStatus = startStatus + ostAdd; 
-end
-
-if nargin < 4 || isempty(uevent1name), uevent1name = 'segStart'; end
-if nargin < 5 || isempty(uevent2name), uevent2name = 'segEnd'; end
 
 %%
-% Load in data
-fprintf('Loading trial data... ');
-load(fullfile(dataPath,'data.mat'))
-% load(fullfile(dataPath,'expt.mat')) % RK 02/02/2021: why is this here? aren't you loading in the expt in the
-% args? 
-fprintf('Done\n');
-
 % get number of trials
-ntrials = expt.ntrials;
+ntrials = length(data); 
 
 % Get sampling rates, etc. 
 fs = expt.audapterParams.sr; 
 frameLength = expt.audapterParams.frameLen; 
 ostFactor = fs/frameLength; 
 
-%% Set up OST recognition 
-% Get the buffer amount for the ostStatBegin (so hand correction is more intuitive) 
-ostWorking = fullfile(get_gitPath, 'current-studies', expt.trackingFileLoc, [expt.trackingFileName 'Working.ost']); 
-ostStatBeginPrev = startStatus - 2; % this is currently true for all cases
-ostStatEndPrev = endStatus - 2; 
+%% Call get_segmentDuration 
 
-% If a working copy doesn't exist, make one
-if exist(ostWorking,'file') ~= 2
-    refreshWorkingCopy(expt.trackingFileLoc, expt.trackingFileName, 'ost');
+for s = 1:nStatusPairs
+    durInfo(s,:) = get_segmentDuration(expt, data, statuses(s), statuses(s+1)); 
 end
 
-% Open file and load file line by line into structure finfo
-fid = fopen(ostWorking,'r');
-tline = fgetl(fid);
-itrial = 1;
-clear finfo
-finfo{itrial} = tline;
-while ischar(tline)
-    itrial = itrial+1;
-    tline = fgetl(fid);
-    finfo{itrial} = tline;
-end
-fclose(fid);
-
-% Lags induced by status waiting for the appropriate amount of either time or frames
-[preStartStatHeur, ~, preStartStatParam2] = get_ost(expt.trackingFileLoc, expt.trackingFileName, startStatus-2); 
-% Unless you're using a stretch/span heuristic in which case it will be the number of frames in the 3rd component / framelen
-if contains(preStartStatHeur,'STRETCH')
-    ostStatBeginPrev_lag = preStartStatParam2 * expt.audapterParams.frameLen / expt.audapterParams.sr; 
-else
-    ostStatBeginPrev_lag = preStartStatParam2; 
-end
-
-% now again for the ending ost
-[preEndStatHeur, ~, preEndStatParam2] = get_ost(expt.trackingFileLoc, expt.trackingFileName, endStatus-2); 
-if contains(preEndStatHeur,'STRETCH')
-    ostStatEndPrev_lag = preEndStatParam2 * expt.audapterParams.frameLen / expt.audapterParams.sr; 
-else
-    ostStatEndPrev_lag = preEndStatParam2; 
-end
+% This creates a structure where the second dimension is number of trials and the first is number of status pairs
 
 
 %% Create user event times
@@ -118,7 +117,12 @@ end
 
 for itrial = 1:ntrials
     % Set up savepaths 
-    savefile = fullfile(dataPath,trialfolder,sprintf('%d.mat',itrial));
+    if isfield(data, 'trial')
+        trialno = data(itrial).trial; 
+    else
+        trialno = itrial; 
+    end
+    savefile = fullfile(dataPath,trialfolder,sprintf('%d.mat',trialno));
     
     % Assume that nothing exists. It's fine for now...   
     sigproc_params = get_sigproc_defaults;
@@ -127,42 +131,40 @@ for itrial = 1:ntrials
     
     event_params = get_event_defaults;
     sigmat = [];
-    trialparams = struct(); 
+    trialparams = struct();     
+    trialparams.event_params.is_good_trial = sum([durInfo(:,itrial).bBadTrack]); % Can change this if you want when you're segmenting
     
-    % Find start and end times of segment of interest
-    ost_stat = data(itrial).ost_stat; 
-    segStartIx = find((ost_stat == startStatus),1); 
-    segEndIx = find((ost_stat == endStatus),1); 
+    % First event---first row
+    ueventTime = zeros(1, length(statuses)); 
+    eventBuffer = zeros(1, length(statuses)); 
     
-    % if the right osts failed to trigger
-    if ~isempty(segStartIx)
-        segStartTime = segStartIx/ostFactor;         
-    else 
-        segStartTime = (length(ost_stat)/2)/ostFactor; % arbitrarily picking midpoint of the file
-        fprintf('OST didn''t trigger: trial %d\n',itrial); 
-    end    
-    if ~isempty(segEndIx)
-        segEndTime = segEndIx/ostFactor; 
-    else
-        segEndTime = segStartTime + 0.05; % can't define this totally arbitrarily because it has to be after startSegTime regardless of the existence of startSegIx
-        fprintf('No status change after trigger: trial %d\n',itrial); 
+    ueventTime(1) = durInfo(1,itrial).adjusted_startStatusTime; 
+    eventBuffer(1) = durInfo(1,itrial).startStatusTime - durInfo(1,itrial).adjusted_startStatusTime; 
+    if isnan(ueventTime(1))
+        ueventTime(1) = (length(ost_stat)/2)/ostFactor; % arbitrarily picking midpoint of the file
+        eventBuffer(1) = 0; 
+        fprintf('OST status %d didn''t trigger on trial %d\n',statuses(1), trialno);
     end
     
-    % The start will have to be PLOTTED at a different time to visually account for the lag in detecting (burst, etc.) 
-    segBeginTimePlot = segStartTime - ostStatBeginPrev_lag; 
-    segEndTimePlot = segEndTime - ostStatEndPrev_lag; 
-    
-    % Generate trialparams and sigmat??? 
-    % Might be able to put this info in the OST file itself... 
-    event_params.user_event_times = [segBeginTimePlot segEndTimePlot];
+    % Use the end time from both rows for all other events
+    for s = 1:nStatusPairs
+        ueventTime(s+1) = durInfo(s, itrial).adjusted_endStatusTime; 
+        eventBuffer(s+1) = durInfo(s,itrial).endStatusTime - durInfo(s,itrial).adjusted_endStatusTime;
+        if isnan(ueventTime(s+1))
+            ueventTime(s+1) = ueventTime(s) + 0.025; % arbitrarily adding 25 ms to previous status
+            eventBuffer(s+1) = 0; 
+            fprintf('OST status %d didn''t trigger on trial %d\n',statuses(1), trialno);
+        end
+    end
+ 
+    % Put into into event_params 
+    event_params.user_event_times = ueventTime;
     if strcmp(expt.trackingFileName,'capper') || strcmp(expt.trackingFileName,'gapper') || strcmp(expt.trackingFileName,'ada') || strcmp(expt.trackingFileName,'ata')
-        uevent1name = 'cBurst'; 
-        uevent2name = 'vStart'; 
+        ueventNames = {'cBurst' 'vStart'}; 
     elseif strcmp(expt.trackingFileName, 'sapper') || strcmp(expt.trackingFileName, 'zapper')
-        uevent1name = 'cStart';
-        uevent2name = 'vStart'; 
+        ueventNames = {'cStart' 'vStart'}; 
     end
-    event_params.user_event_names = {uevent1name uevent2name}; 
+    event_params.user_event_names = ueventNames; 
     
     trialparams.sigproc_params = sigproc_params; 
     trialparams.plot_params = plot_params; 
@@ -178,36 +180,36 @@ end
     audioGUI(dataPath,[1:ntrials],'signalIn',[],0); % 0 for no savecheck (yes, you want to overwrite every time) 
     % check for two events, if there are any then open those in audioGUI 
     
-    % do a gen_dataVals? 
-    avail2perturb = [];
+    % Generate vector of true durations and available durations
+    availableDur = nan(nStatusPairs, ntrials); 
+    trueDur = nan(nStatusPairs, ntrials);  
     goodTokens = 0; 
     for itrial = 1:ntrials
         load(fullfile(dataPath,trialfolder,sprintf('%d.mat',itrial))); 
         if trialparams.event_params.is_good_trial
-            segStartTime = trialparams.event_params.user_event_times(1); 
-            segEndTime = trialparams.event_params.user_event_times(2); 
-        
-            % Readjust the time for the amount of time that you ACTUALLY have available to you for warping purposes 
-            segStartTime_readjust = segStartTime + ostStatBeginPrev_lag; 
-            segEndTime_readjust = segEndTime + ostStatEndPrev_lag;      
-        
-            avail2perturb(itrial) = segEndTime_readjust - segStartTime_readjust; 
+            for s = 1:nStatusPairs
+                % Get pairs of event times, adjust the starting one by putting the buffer back 
+                startTime = trialparams.event_params.user_event_times(s); 
+                endTime = trialparams.event_params.user_event_times(s+1); 
+                adjusted_startTime = startTime + eventBuffer(s); 
+                
+                availableDur(s, itrial) = endTime - adjusted_startTime; 
+                trueDur(s, itrial) = endTime - startTime; 
+            end
             goodTokens = goodTokens + 1; 
         else
-            avail2perturb(itrial) = NaN; 
+            availableDur(s, itrial) = NaN; 
+            trueDur(s, itrial) = NaN; 
         end
         
     end
     
-    % spit out values 
-    availMean = nanmean(avail2perturb); 
-    availMax = nanmax(avail2perturb); 
-    availSd = nanstd(avail2perturb); 
-    
-    % put them into expt.durHoldMax
-    durHoldMax = availMean + (2*availSd);
-    expt.durHoldMax = durHoldMax;
-    expt.goodTokens = goodTokens; 
-    expt.avail2perturb = avail2perturb; 
+    % put them into expt
+    for s = 1:nStatusPairs
+        intervalName = durationNames{s}; 
+        expt.availableDur.(intervalName) = availableDur(s,:); 
+        expt.trueDur.(intervalName) = trueDur(s,:); 
+    end
+
     
 end
