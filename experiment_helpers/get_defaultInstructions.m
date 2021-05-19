e6a9b9b22fb43ce3afda83ba61344b14dc042d07
Dinglan Tang <dtang38@wisc.edu>
2021-05-19 14:58:40 -0500

    varmodMEG_listening intruction

--- experiment_helpers/get_defaultInstructions.m
+++ experiment_helpers/get_defaultInstructions.m
@@ -2,7 +2,9 @@ function [instruct] = get_defaultInstructions()
 %GET_DEFAULTINSTRUCTIONS  Return default experiment instructions
 
 instruct.introtxt = {'Read each word out loud as it appears.' '' 'Press the space bar to continue when ready.'};
+instruct.introtxtlisten = {'Listen to a recording of the presented word.' '' 'Press the space bar to continue when ready.'};
 instruct.readytxt = 'Get ready to SPEAK.';
+instruct.readylisten = 'Get ready to LISTEN';
 instruct.breaktxt = 'Time for a break!\n\n%d of %d trials done.\n\n\n\nPress the space bar to continue.';
 instruct.thankstxt = 'Thank you!\n\n\n\nPlease wait.';
 instruct.waittxt = 'Please wait.';
