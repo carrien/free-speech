0050c2e228f24253e115e0a6e2d366aa67afb57b
Robin Karlin <rkarlin@wisc.edu>
2022-01-04 12:59:47 -0600

    changed to allow using subset of trials if multiple OSTs

--- experiment_helpers/adjustOsts.m
+++ experiment_helpers/adjustOsts.m
@@ -1,12 +1,31 @@
-function [] = adjustOsts(expt, h_fig)
+function [] = adjustOsts(expt, h_fig, word, trackingFileName)
 % This function will be used by the button "Adjust OSTs" in the control window of an experiment display. The purpose of this
 % is to adjust the OST files for a participant mid-experiment, if they have changed their speech such that the OSTs are no
 % longer working (especially talking louder or something, where your thresholds may no longer be accurate)
+% 
+% INPUT ARGUMENTS: 
+%       expt:           
+%           expt structure from the experiment. For the sake of audpater_viewer this turns into exptOst (so it can be saved,
+%           and then resaved over with the originals)
+%       h_fig:
+%           Figure handle from the experiment, so pause text can be appropriately included 
+%       word:           
+%           the word to look for in expt.inds.words. If empty or does not exist, just assumes that all trials should be used.
+%           Use this when you are in an experiment that has multiple OST files, such as taimComp. If you have multiple words
+%           that use one OST file (e.g., both "size" and "sigh" use some OST file called "sigh"), you can put in a cell array
+%           {'size' 'sigh'} and the indices from expt.inds.words.size and expt.inds.words.sigh will be concatenated
+%       ostFN:       
+%           the name of the OST file, same use patterns as other trackingFileName arguments (i.e., 'size' would look for
+%           sizeWorking in expt.trackingFileLoc). If this is empty, and word is also empty, just uses expt.trackingFileName.
+%           If it is empty and word is NOT empty, uses the first word in word. 
+%           
 % 
 % Process: 
 % 1. Takes in expt and h_fig (so that you can run a split version of pause_trial) 
 % 2. Compiles data from last 18 temp trials into one data file so it can be read by audapter_viewer all at once (if you don't
 % have 18, it will just take the last however many you have) 
+% --- If you are in an experiment that has multiple OST files (e.g. taimComp), the argument "trialSubset" will tell the
+% script which trials are acceptable
 % 3. Opens audapter_viewer with that data file and the expt 
 % 4. Use audapter_viewer as normal 
 % 5. Marks last temporary trial 1 for bChangedOsts
@@ -19,10 +38,36 @@ function [] = adjustOsts(expt, h_fig)
 % Accompanies function add_adjustOstButton to add a button to control screen to get to this script
 % 
 % Initiated RPK 6/2/2021
+% Major change RPK 2022-01-03 to accommodate multiple OST files, for taimComp
 % 
 % 
 
 dbstop if error 
+%% Default arguments
+
+if nargin < 3 || isempty(word), word = ''; end
+if nargin < 4 || isempty(trackingFileName)
+    if nargin < 3 || isempty(word)
+        if ~isfield(expt, 'trackingFileName')
+            trackingFileName = 'measureFormants'; 
+        else
+            trackingFileName = expt.trackingFileName; 
+        end
+    elseif iscell(word)
+        trackingFileName = word{1}; 
+    else
+        trackingFileName = word; 
+    end
+end
+
+% Change string inputs in word to cell 
+if ischar(word)
+    word = {word}; 
+end
+
+% Set exptOst so that you don't lose original expt 
+exptOst = expt; 
+exptOst.trackingFileName = trackingFileName; 
 
 %% Display faux pause information (without actually pausing) 
 get_figinds_audapter;
@@ -49,12 +94,12 @@ CloneFig(h_fig(stim),h_fig(dup))
 nCompiledTrials = 18; 
 
 % OST file information 
-if (~isfield(expt, 'trackingFileDir') && ~isfield(expt, 'trackingFileLoc')) 
+if (~isfield(exptOst, 'trackingFileDir') && ~isfield(exptOst, 'trackingFileLoc')) 
     trackingFileDir = 'experiment_helpers'; 
-elseif isfield(expt, 'trackingFileDir')
-    trackingFileDir = expt.trackingFileDir; 
-elseif isfield(expt, 'trackingFileLoc')
-    trackingFileDir = expt.trackingFileLoc; 
+elseif isfield(exptOst, 'trackingFileDir')
+    trackingFileDir = exptOst.trackingFileDir; 
+elseif isfield(exptOst, 'trackingFileLoc')
+    trackingFileDir = exptOst.trackingFileLoc; 
 end
 
 if strcmp(trackingFileDir, 'experiment_helpers')
@@ -63,19 +108,13 @@ else
     ostPath = get_exptRunpath(trackingFileDir); 
 end
 
-if ~isfield(expt, 'trackingFileName')
-    trackingFileName = 'measureFormants'; 
-else
-    trackingFileName = expt.trackingFileName; 
-end
-
 ostWorking = fullfile(ostPath, [trackingFileName 'Working.ost']); 
  
 
 %% Get temporary data files
 
 % Look for temporary trial directory
-tempdirs = regexp(genpath(expt.dataPath),'[^;]*temp_trials','match')';
+tempdirs = regexp(genpath(exptOst.dataPath),'[^;]*temp_trials','match')';
 if isempty(tempdirs)
     % If there isn't one
     fprintf('No trials left to adjust OSTs for.\n')
@@ -85,18 +124,26 @@ elseif length(tempdirs) > 1
     return;
 end
 
-compiledData = []; 
-
 tempdir = tempdirs{1}; 
 trialnums = get_sortedTrials(tempdir);
 
+% Get trials in the allowed subset (e.g., just ones in expt.inds.words.sigh)
+allowedTrials = []; 
+for w = 1:length(word)
+    allowedTrials = [allowedTrials exptOst.inds.words.(word{w})]; 
+end
+trialnums = intersect(allowedTrials, trialnums); % Get the intersection of the subset of trials and the completed trials in temp
+
 % Get last nCompiledTrials trials (finds the integers so that you get a full indexed list) 
 trials2compile = find(round(trialnums) == trialnums, nCompiledTrials, 'last'); 
-trials2compile = trialnums(trials2compile); % Just in case your trial list for some reason doesn't start at 1 
+trials2compile = trialnums(trials2compile); % Just in case your trial list for some reason doesn't start at 1 ... like if you're only doing a subset
 
+% Compile the data into a data.mat
+compiledData = []; 
 for t = 1:length(trials2compile)
     trialNo = trials2compile(t); 
     load(fullfile(tempdir, [num2str(trialNo) '.mat']), 'data'); 
+    data.trialNo = trialNo; 
     compiledData = [compiledData; data]; 
 end
     
@@ -108,12 +155,12 @@ save(fullfile(tempdir, [num2str(max(trials2compile)), '.mat']), 'data');
 % Clear data and save full compiled data as data_ostChange.mat
 clear data; 
 data = compiledData; 
-save(fullfile(expt.dataPath, 'data_ostChange.mat'), 'data'); % Unclear if we actually need to save this
+save(fullfile(exptOst.dataPath, 'data_ostChange.mat'), 'data'); % Unclear if we actually need to save this
 
 %% Open audapter viewer
 
 % Using data (which is now compiledData)
-audapter_viewer(data,expt); 
+audapter_viewer(data,exptOst); 
 hGui = findobj('Tag','audapter_viewer'); 
 
 % Pause the experiment
@@ -134,6 +181,10 @@ set(h_fig(dup),'CurrentCharacter','@')
 % Send in new values to audapter before resetting text
 Audapter('ost', ostWorking, 0);
 fprintf('New OST values fed into Audapter.\n')
+
+% Resave expt
+save(fullfile(expt.dataPath, 'expt.mat'), 'expt'); % Because in some cases you may have changed expt to have a different trackingFileName 
+fprintf('Original expt structure saved'); 
 
 % Refresh text
 delete_exptText(h_fig, h_text)
