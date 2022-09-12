ec17b146b1d0576d6ebf0e92a2dd3b5489218af3
Chris Naber <cwnaber@wisc.edu>
2022-09-12 15:55:08 -0500

    integrated local fallback into function

--- experiment_helpers/get_cbPermutation.m
+++ experiment_helpers/get_cbPermutation.m
@@ -1,4 +1,4 @@
-function [permIx,conditions] = get_cbPermutation(exptName, permsPath, population, permIx)
+function [permIx,conditions] = get_cbPermutation(exptName, permsPath, population, permIx, bLocalFallback, warnDataPath)
 % Gets the index of the row you want to use for your participant for counterbalancing purposes and the
 % conditions (in order) that that row has. 
 % 
@@ -17,6 +17,8 @@ function [permIx,conditions] = get_cbPermutation(exptName, permsPath, population
 
 if nargin < 2 || isempty(permsPath), permsPath = get_exptLoadPath(exptName); end  
 if nargin < 3 || isempty(population), population = ''; end
+if nargin < 5 || isempty(bLocalFallback), bLocalFallback = 0; end
+
 
 if isempty(population)
     permsFile = ['cbPermutation_' exptName '.mat']; 
@@ -24,16 +26,33 @@ else
     permsFile = ['cbPermutation_' exptName '_' population '.mat']; 
 end
 
+bUsedLocalFallback = 0;
 if ~exist(fullfile(permsPath, permsFile),'file')
-    error('No counterbalancing file in this directory (%s)', permsPath); 
+    if bLocalFallback
+        warning('No counterbalancing file in this directory (%s) - trying local path instead...', permsPath);
+        localPermsPath = get_exptLocalPath(exptName);
+        if ~exist(fullfile(localPermsPath, permsFile),'file')
+            error('No counterbalancing file in this local directory either (%s).', localPermsPath);
+        else
+            disp('Found local directory.')
+            permsPath = localPermsPath;
+        end
+        bUsedLocalFallback = 1;
+    else
+        error('No counterbalancing file in this directory (%s)', permsPath);
+    end
 end
 
 perms = load(fullfile(permsPath, permsFile)); 
 varField = fieldnames(perms); 
 cbPermutation = perms.(char(varField));
 
-[~,countCol] = size(cbPermutation); % Find the column that counts the number of uses
+[nRows,countCol] = size(cbPermutation); % Find the column that counts the number of uses
 lastCondCol = countCol-1; 
+
+if bUsedLocalFallback
+    permIx = randi(nRows); %get a random index from the set
+end
 
 rng('shuffle')
 if nargin < 4 || isempty(permIx)
@@ -44,4 +63,14 @@ else
     conditions = cbPermutation(permIx, 1:lastCondCol); 
 end
 
+% raise warning that includes permIx
+if bUsedLocalFallback
+    % TODO make sure this works
+    warningFile = fullfile(warnDataPath,'cb_get_warning.txt');
+    fid = fopen(warningFile, 'w');
+    warning('Server did not respond. Using randomly generated permutation index: %d (see warning file)', permIx);
+    fprintf(fid,'Server did not respond. Random permIx generated: %d', permIx);
+    fclose(fid);
 end
+
+end %EOF
