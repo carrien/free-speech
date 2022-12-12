31ed652c124eba862504d76053e5922a67ed917d
Chris Naber <cwnaber@wisc.edu>
2022-12-12 08:13:08 -0600

    rename expFunStr. reorder case checks. use str2func not eval

--- experiment_helpers/get_experiment_function.m
+++ experiment_helpers/get_experiment_function.m
@@ -6,10 +6,10 @@ switch expName
         expFun = @run_speak_ptb;
     case 'colorName'
         expFun = @run_colorname_ptb;
-    case 'uhdapter_down'
-        expFun = @run_uhdapter_audapter_down;        
     case {'uhdapter' 'stress'}
         expFun = @run_uhdapter_audapter;
+    case 'uhdapter_down'
+        expFun = @run_uhdapter_audapter_down;        
     case {'vsaAdapt', 'vsaAdapt2'}
         expFun = @run_vsaAdapt_audapter;
     case {'varModIn', 'varModOut', 'varModOut2', 'varModInOut'}
@@ -18,14 +18,14 @@ switch expName
         expFun = @run_varMod_audapter_blocked;
     otherwise
         % see if there's a function with the format run_'expName'_audapter
-        expFun = sprintf('run_%s_audapter', expName);
-        isFunction = which(expFun);
+        expFunStr = sprintf('run_%s_audapter', expName);
+        isFunction = which(expFunStr);
         if isFunction
             fprintf(['No explicit function name found in list in get_experiment_function.m\n' ...
-                'Using default behavior to set function name to %s\n'], expFun)
-            expFun = eval(sprintf('@%s', expFun)); % convert from string to function_handle
+                'Using default behavior to set function name to %s\n'], expFunStr)
+            expFun = str2func(sprintf('@%s', expFunStr)); % convert from string to function_handle
         else
-            fprintf('Function for experiment ''%s'' not found.',expName)
+            fprintf('Function for experiment ''%s'' not found.\n',expName)
             expFun = [];
         end
 end
