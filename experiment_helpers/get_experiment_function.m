7105c93cae2bf5eb3684d5d801ae4e0dfaf95dc1
Chris Naber <50926971+cwnaber@users.noreply.github.com>
2022-12-12 09:07:02 -0600

    create default option that doesn't just return empty (#77)
    * create default option that doesn't just return empty
    
    * remove cases that adhere to default
    
    * rename expFunStr. reorder case checks. use str2func not eval

--- experiment_helpers/get_experiment_function.m
+++ experiment_helpers/get_experiment_function.m
@@ -2,63 +2,30 @@ function expFun = get_experiment_function(expName)
 %For a given experiment name, return the function that runs that experiment
 
 switch expName
-    case 'noisyfb'
-        expFun = @run_noisyfb_audapter;
     case 'stroopVOT'
         expFun = @run_speak_ptb;
     case 'colorName'
         expFun = @run_colorname_ptb;
-    case 'uhdapter'
+    case {'uhdapter' 'stress'}
         expFun = @run_uhdapter_audapter;
     case 'uhdapter_down'
         expFun = @run_uhdapter_audapter_down;        
-    case 'stress'
-        expFun = @run_uhdapter_audapter;
-    case 'vin'
-        expFun = @run_vin_audapter;
-    case 'vinRep'
-        expFun = @run_vinRep_audapter;
-    case 'sAdapt'
-        expFun = @run_sAdapt_audapter;
     case {'vsaAdapt', 'vsaAdapt2'}
         expFun = @run_vsaAdapt_audapter;
     case {'varModIn', 'varModOut', 'varModOut2', 'varModInOut'}
         expFun = @run_varMod_audapter;
     case 'varModMEG'
         expFun = @run_varMod_audapter_blocked;
-    case 'timeAdapt'
-        expFun = @run_timeAdapt_audapter; 
-    case 'cif1'
-        expFun = @run_cif1_audapter;
-    case 'vsaGeneralize'
-        expFun = @run_vsaGeneralize_audapter;
-    case 'attentionComp'
-        expFun = @run_attentionComp_audapter;
-    case 'simonSingleWord'
-        expFun = @run_simonSingleWord_audapter;
-    case 'taimComp'
-        expFun = @run_taimComp_audapter;        
-    case 'coAdapt'
-        expFun = @run_coAdapt_audapter;
-    case 'attentionAdapt'
-        expFun = @run_attentionAdapt_audapter;
-    case 'simonMultisyllable'
-        expFun = @run_simonMultisyllable_audapter;
-    case 'simonMultisyllable_v2'
-        expFun = @run_simonMultisyllable_v2_audapter;
-    case 'simonToneLex'
-        expFun = @run_simonToneLex_audapter;
-    case 'simonSyllableTransfer'
-        expFun = @run_simonSyllableTransfer_audapter;
-    case 'noGoAdapt'
-        expFun = @run_noGoAdapt_audapter;
-    case 'simonHomophonePic'
-        expFun = @run_simonHomophonePic_audapter;
-    case 'simonHomophoneWord'
-        expFun = @run_simonHomophoneWord_audapter;
-    case 'simonHomophone'
-        expFun = @run_simonHomophone_audapter;
     otherwise
-        fprintf('Function for experiment ''%s'' not found.',expName)
-        expFun = [];
+        % see if there's a function with the format run_'expName'_audapter
+        expFunStr = sprintf('run_%s_audapter', expName);
+        isFunction = which(expFunStr);
+        if isFunction
+            fprintf(['No explicit function name found in list in get_experiment_function.m\n' ...
+                'Using default behavior to set function name to %s\n'], expFunStr)
+            expFun = str2func(sprintf('@%s', expFunStr)); % convert from string to function_handle
+        else
+            fprintf('Function for experiment ''%s'' not found.\n',expName)
+            expFun = [];
+        end
 end
