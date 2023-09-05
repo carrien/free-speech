fb37a208b0358f1fdef3d7339803080e96233f59
Chris Naber <50926971+cwnaber@users.noreply.github.com>
2023-09-05 17:12:16 -0500

    Fix default checkMethod
    Co-authored-by: Carrie Niziolek <cniziolek@wisc.edu>

--- experiment_helpers/set_exptDefaults.m
+++ experiment_helpers/set_exptDefaults.m
@@ -129,7 +129,7 @@ amplcalc.ons_thresh = 0.01;
 amplcalc.offs_thresh = 0.015;
 
 % used with newer experiments
-amplcalc.checkMethod = 'mean';   % Compare mean RMS during vowel against amplcalc.rmsThresh
+amplcalc.checkMethod = 'peak_window';   % Compare mean RMS during vowel against amplcalc.rmsThresh
 amplcalc.peakWindowSecs = 0.200; % Number of seconds of buffer to include surrounding the peak amplitude to find RMS value
 amplcalc.rmsThresh = 0.037;      % RMS values below this trigger "speak louder" prompt. 0.037 is ~78.5 dBA on SMNG hardware
     % expt.amplcalc.limits is a 2x2 array structured like this:
