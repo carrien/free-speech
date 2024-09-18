baf8e8f7b5d9410401abeee6510c39e2040a3679
Chris Naber <50926971+cwnaber@users.noreply.github.com>
2024-09-18 15:25:03 -0500

    Update check_audioLevels.m

--- experiment_helpers/check_audioLevels.m
+++ experiment_helpers/check_audioLevels.m
@@ -1,13 +1,17 @@
 function check_audioLevels(what2check)
 if nargin < 1
-        what2check = questdlg({'do you want to check'; ...
-        '(a): that the mic and headphones are working, then check background noise levels (before participant arrives)';...
-        '(b): the microphone level with the participant'},'What do you want to check?','(a)','(b)','(a)');
+    what2check = 'a';
 end
 
 %% set up screens
 h_fig = setup_exptFigs;
 get_figinds_audapter; % names figs: stim = 1, ctrl = 2, dup = 3;
+
+% close ctrl screen and enlarge experimenter's duplicated screen
+close(h_fig(ctrl)); 
+dup_position = [0 0.08 0.8 0.9]; %experimenter screen position
+set(h_fig(3),'OuterPosition',dup_position);
+
 
 %% initialize Audapter 
 audioInterfaceName = 'Focusrite USB'; %SMNG default for Windows 10
@@ -33,49 +37,36 @@ AudapterIO('init', p);
 
 %% run test
 switch what2check
-    case '(a)'
-        %% test mic and headphones
-        % give instructions and wait for keypress
-        Audapter('reset'); 
-        Audapter('start'); 
-        h_ready = draw_exptText(h_fig,-.3,.5,{'Testing mic and headphones.';'Make sure you can hear your voice played back';'Press space to end test'},'Color','white','FontSize',35);
-        pause
-        Audapter('stop');
-        delete_exptText(h_fig,h_ready)
-        
+    case 'a'
         %% test headphone level
         Audapter('reset'); 
         Audapter('start'); 
-        h_ready = draw_exptText(h_fig,-.3,.5,sprintf(['Level testing has started.\n\n' ...
-            'Be sure headphones are plugged into output 2 of headphone amplifier.\n\n' ...
-            'Make sure SPL meter settings are:\n\t\tNo max or min\n\t\tA mode\n\t\tSlow\n\t\tLevel 50-100 db\n\n' ...
-            'Place SPL meter in headphones without foam cover.\n' ...
-            'Noise level should be ~60dB.\n' ...
-            'If levels are off, adjust "pp headphones" knob on headphone amplifier.\n\n' ...
-            'Press any key to stop once levels are confirmed.']),'Color','white','FontSize',35);
+        h_ready = draw_exptText(h_fig,-.4,.5,sprintf(['Testing mic and headphones.\n\n' ...
+            'Speak into mic. You should hear noise and speech in headphones.\n\n' ...
+            'Set SPL meter to:\n\t\tNo max or min\n\t\tA mode\n\t\tSlow\n\t\tLevel 50-100 db\n\n' ...
+            'Place SPL meter in headphones.\n' ...
+            'Noise level should be ~60dB\n' ...
+            'If not, adjust "pp headphones" knob on headphone amplifier.\n\n' ...
+            'Press any key to stop.']),'Color','white','FontSize',35);
         pause
         Audapter('stop');
-        delete_exptText(h_fig,h_ready)
 
-    case '(b)'
+    case 'b'
         %% test headphone level
         Audapter('reset'); 
         Audapter('start'); 
         get_figinds_audapter;
         figure(h_fig(dup));
-        h_ready_dup = text(-0.4, 0.5, sprintf(['Level testing has started.\n\n' ...
-            'Be sure headphones are plugged into output 2 of headphone amplifier.\n\n' ...
-            'Make sure SPL meter settings are:\n\t\tNo max or min\n\t\tA mode\n\t\tSlow\n\t\tLevel 50-100 db\n\n' ...
+        h_ready_dup = text(-0.4, 0.5, sprintf(['Testing mic amplitude.\n\n' ...
+            'Set SPL meter to:\n\t\tNo max or min\n\t\tA mode\n\t\tSlow\n\t\tLevel 50-100 db\n\n' ...
             'Place SPL meter in headphones without foam cover.\n' ...
             'Noise level should be ~80db while pp says "head".\n' ...
-            'Adjust the microphone gain ("experiment mic") if this is too low or too high.\n\n' ...
-            'Press any key to stop once levels are confirmed.']), 'Color','white','FontSize',35);
+            'If not, adjust microphone gain ("experiment mic").\n\n' ...
+            'Press any key to stop.']), 'Color','white','FontSize',35);
         figure(h_fig(stim));
         h_ready_stim = text(0, 0.5, sprintf('Please say "head" with the vowel stretched out \n                until the noise goes away.'), 'Color','white','FontSize',35);
         pause
         Audapter('stop');
-        delete_exptText(h_fig,h_ready_dup)
-        delete_exptText(h_fig,h_ready_stim)
 end
        
         %% clean up
