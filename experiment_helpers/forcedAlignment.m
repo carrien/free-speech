08acdd345e05fb9b93cd9b75fec4c1e1b6b68391
Chris Naber <50926971+cwnaber@users.noreply.github.com>
2024-10-01 14:35:04 -0500

    mfa wip

--- experiment_helpers/forcedAlignment.m
+++ experiment_helpers/forcedAlignment.m
@@ -1,155 +1,168 @@
-function [] =  forcedAlignment(dataPath,exptfield, trialNums)%, language)
+function [] =  forcedAlignment(params)
+% TODO header
 
-%% LANGUAGE FLAG IN PROGRESS; don't use the French dictionary. 'oe' - 'neuf'
+% 2024-10 CWN compiled into one function
 
+%% set parameters
+defaultParams.version = 3;
+defaultParams.dataPath = pwd;
+defaultParams.cmdLocation = '%windir%\System32\cmd.exe';
+defaultParams.activatePath = fullfile('C:\ProgramData\miniconda3\Scripts\activate.bat');
+defaultParams.exptfield = 'listWords';
+defaultParams.wordlist = [];
+defaultParams.genfilesOrAlign = 'both';
+if ismac
+    defaultParams.alignerLocation = '/Applications/montreal-forced-aligner';
+elseif ispc
+    defaultParams.alignerLocation = 'C:\Users\Public\Documents\software\montreal-forced-aligner';
+end
+if nargin < 1, params = []; end
+params = set_missingFields(params, defaultParams, 0);
 
-% % FORCEDALIGNMENT Use the montreal forced aligner on experiment data
-% %   FORCEDALIGNMENT(DATAPATH,EXPTFIELD)
-% %   Generates WAV and TXT files for each data value in the DATAPATH
-% %   directory's data.mat file, then runs the aligner on those files.
-% %   This path must contain a file called data.mat with each trial n stored
-% %   in data(n). Needs expt.mat to make txt file. EXPTFIELD is the parameter
-% %   that corresponds to the word being said in the WAV file. (ex. Stroop
-% %   expriements may need to use the color rather than the word shown to
-% %   the participant)
-
-%  ---> Indicates a line of code that is commented out
-% %  ---> Indicates a comment
-
-% % Handle various user-given arguments
-% % TODO: Allow for the manual indication of pre/post alignment folder
-% % locations via input arguments
-dbstop if error
-
-if nargin < 1 || isempty(dataPath), dataPath = cd; end
-
-
+% load data
 fprintf('Loading data... ');
-% % load data
-load(fullfile(dataPath,'data.mat'), 'data');
-load(fullfile(dataPath,'expt.mat'), 'expt');
+load(fullfile(params.dataPath,'data.mat'), 'data');
+load(fullfile(params.dataPath,'expt.mat'), 'expt');
 fprintf('done.\n');
 
-if nargin < 2 || isempty(exptfield)
-    wordlist = expt.words(expt.allWords);
-elseif iscell(exptfield)
-    wordlist = exptfield;
-elseif isfield(expt, exptfield)
-    wordlist = expt.(exptfield);
-end
-
-if nargin < 3 || isempty(trialNums)
-    trialNums = 1:length(data);
-end
-
-
-%if nargin < 3 || isempty(language), language = 'english'; end
-% % Determine whether or not the script is being run on a Mac or PC,
-% % set the correct expected location of the montreal forced aligner files.
-
-if ismac
-    alignerLocation = '/Applications/montreal-forced-aligner';
-elseif ispc
-    alignerLocation = 'C:\Users\Public\Documents\software\montreal-forced-aligner';
-end
-
-% % Handle the incorrect placement of the aligner/dictionary files during installation
-
-if ~exist(alignerLocation, 'dir')
-    disp("The montreal-forced-aligner folder has not been placed in the proper directory on this machine, the script will now exit");
-    return
+% set wordlist
+if isempty(params.wordlist)
+    params.wordlist = expt.(params.exptfield);
 else
-    %    if strcmpi(language, 'french')
-    %        dictionary = fullfile(alignerLocation,'fr.dict.txt');
-    %    elseif strcmpi(language,'german')
-    %        dictionary = fullfile(alignerLocation,'de.dict.txt');
-    %    else
-    dictionary = fullfile(alignerLocation,'librispeech-lexicon.txt');
-    %    end
-    if ~exist(dictionary, 'file')
-        disp("The librispeech-lexicon dictionary file has not been placed in the montreal-forced-aligner folder, the script will now exit");
-        return
-    end
+    % You specified `wordlist` already. It should be a cell array of strings
 end
 
-% % Create variables that hold the locations to:
-% %     - A folder for the generated WAV/LAB files (called "PreAlignment")
-% %     - The executable mfa_align file that performs the alignment
-% %     - A folder for the generated TEXTGRID files (called "PostAlignment")
-
-filename = fullfile(dataPath,'PreAlignment');
-if ~exist(filename, 'dir')
-    mkdir(filename)
+% finish setting params
+defaultParams.trialNums = 1:length(data);
+switch params.version
+    case 1
+        defaultParams.dictionary = 'english_us_arpa';
+        defaultParams.language = 'english_us_arpa';
+    case 3
+        defaultParams.dictionary = 'librispeech-lexicon.txt';
+        defaultParams.language = 'english';
 end
-outputLocation = fullfile(dataPath,'PostAlignment');
-if ~exist(outputLocation, 'dir')
-    mkdir(outputLocation)
+params = set_missingFields(params, defaultParams, 0);
+
+%% make folders
+prealignFolder = fullfile(params.dataPath,'PreAlignment');
+if ~exist(prealignFolder, 'dir')
+    mkdir(prealignFolder)
 end
-executableLocation = fullfile(alignerLocation,'bin','mfa_align');
+postalignFolder = fullfile(params.dataPath,'PostAlignment');
+if ~exist(postalignFolder, 'dir')
+    mkdir(postalignFolder)
+end
 
+%% get sampling rate
+if isfield(data(1).params,'sr')
+    sampleRate = data(1).params.sr;
+else
+    sampleRate = data(1).params.fs;
+end
 
-% % Create a WAV and TXT file for each data value, srFLAG is a sentinel value
-% % for handling the case where the sample rate is < 16000 but the user wants to continue
-% % TODO: Should we even allow this?
-srFlag = false;
+if params.version == 1 && sampleRate < 16000
+    error('MFA version 1 doesn''t work with sampling rates lower than 16 kHz.')
+end
 
-for i=1:length(trialNums)
-    % iTrial and i are identical except when trialNums was passed in.
-    % Then, i is the loop position. iTrial is the trial number.
-    iTrial = trialNums(i); 
-
-    word = upper(string(wordlist(i)));
-    modifiedTxtName = fullfile(filename,sprintf('%s%d%s','AudioData_',iTrial,'.lab'));
-    fid = fopen(modifiedTxtName,'wt');
-    fprintf(fid,'%s',word);
-    fclose(fid);
-    
-    % %      Depending on the experiment, the parameter for the sample rate may be
-    % %      named differently, check for that here
-    signalData = data(iTrial).signalIn;
-    if isfield(data(iTrial).params,'sr')
-        sampleRate = data(iTrial).params.sr;
-    else
-        sampleRate = data(iTrial).params.fs;
-    end
-    
-    
-    % %      Figure out what the user wants to do if the sample rate is
-    % %      unsupported
-    % %      TODO: Maybe try to incorporate the data validator here? If not here,
-    % %      maybe try to validate the data prior to even running the script?
-    
-    if sampleRate < 16000 && srFlag == false
-        prompt = 'Sample rate is less than 16000, may have issues running these files through the aligner, try anyway? y / n ';
-        response = input(prompt);
-        if strcmp(response,'y')
-            srFlag = true;
-            modifiedWavName = fullfile(filename,sprintf('%s%d%s','AudioData_',iTrial,'.wav'));
-            audiowrite(modifiedWavName,signalData,sampleRate);
-            continue
-            
-        elseif strcmp(response,'n')
-            disp("Cleaning up and Exiting script...");
+switch params.version
+    case 1
+        %% MFA version 1
+        % check for files
+        if ~exist(params.alignerLocation, 'dir')
+            disp("The montreal-forced-aligner folder has not been placed in the proper directory on this machine, the script will now exit");
             return
+        else
+            dictionaryPath = fullfile(params.alignerLocation,params.dictionary);
+            if ~exist(dictionaryPath, 'file')
+                fprintf('Couldn''t find file %s. Exiting script.\n', dictionaryPath);
+                return
+            end
+        end
+
+        % make AudioData files
+        makeAudioData
+
+        % run forced aligner
+        executableLocation = fullfile(params.alignerLocation,'bin','mfa_align');
+        command = sprintf('%s %s %s %s %s',executableLocation , prealignFolder, dictionaryPath, params.language, postalignFolder);
+        system(command)
+
+        % user message
+        fprintf('Generated TextGrid files')
+
+    case 2
+        %% MFA version 2
+        disp('MFA version 2 not supported.')
+        return
+    case 3
+        %% MFA version 3
+
+        activateDir = fileparts(params.activatePath);
+        if ~exist(activateDir, 'dir')
+            prompt = sprintf('The aligner environment does not appear to be configured correctly in %s. Try to align anyway?', activateDir);
+            response = askNChoiceQuestion(prompt,{'y' 'n'});
+            if strcmpi(response,'n')
+                fprintf('See the KB doc on installing MFA for instructions on setting up the aligner conda environment \n')
+                return
+            end
+        end
+
+        % make AudioData files
+        makeAudioData
+
+        % run forced aligner
+        fprintf('Calling conda to run MFA... \n');
+
+        % Get current directory so you can return to it
+        currentDir = pwd;
+
+        % Then change working directory to the datapath
+        cd(params.dataPath);
+
+        % Create the mfa command and then copy it to the clipboard so the user can paste it in
+        mfaCommand = sprintf('%s ./%s %s %s ./%s', 'mfa align --clean', 'PreAlignment', params.language, params.dictionary, 'PostAlignment');
+        clipboard('copy', mfaCommand);
+        warning(sprintf(['The alignment command has been copied to your clipboard. \n' ...
+            'When the CMD interface appears in the MATLAB Command Window, paste the command into the command window and hit enter.\n'...
+            'When alignment is complete, type exit, and hit enter'])); %#ok<SPWRN>
+        fprintf('\n\n')
+        askNChoiceQuestion('Enter Y to acknowledge:', {'y' 'Y'}, 0);
+
+        % Run system command
+        systemCommand = sprintf('%s %s %s %s', params.cmdLocation, '"/K"', params.activatePath, params.alignerLocation);
+        systemOutput = system(systemCommand);
+
+        % Back in Matlab
+        fprintf('Conda call completed. Returned to Matlab.\n');
+        cd(currentDir)
+
+        if ~systemOutput
+            disp("Check the console output and PostAlignment folder to be sure that your TextGrid files have been successfully created.");
+        else
+            disp("There was an error. Please read the error message carefully.");
+        end
+end
+
+    function [] = makeAudioData()
+        % iTrial and i are identical except when trialNums was passed in.
+        % Then, i is the loop position. iTrial is the trial number.
+        for i = 1:length(trialNums)
+            % make lab file
+            iTrial = trialNums(i);
+
+            word = upper(string(params.wordlist(i)));
+            modifiedTxtName = fullfile(filename,sprintf('%s%d%s','AudioData_',iTrial,'.lab'));
+            fid = fopen(modifiedTxtName,'wt');
+            fprintf(fid,'%s',word);
+            fclose(fid);
+
+            %make wav file
+            modifiedWavName = fullfile(prealignFolder,sprintf('%s%d%s','AudioData_',i,'.wav'));
+            audiowrite(modifiedWavName,signalData,sampleRate);
         end
     end
-    modifiedWavName = fullfile(filename,sprintf('%s%d%s','AudioData_',iTrial,'.wav'));
-    audiowrite(modifiedWavName,signalData,sampleRate);
-    % %      TODO: Display percent complete?
-end
 
-% % At least in windows, another conditional for mac?
-command = sprintf('%s %s %s %s %s',executableLocation , filename, dictionary, 'english', outputLocation); %language
-system(command)
 
-% % TODO: check whether or not the TextGrids were properly created?
-% % end the alignment part of the script
+end %EOF
 
-% % Clean up
-% % TODO: Delete wav/text files folder (prealignment)
-% % rmdir(filenameChar)?
-
-disp("The TextGrid files have been successfully created and are located in the PostAlignment folder");
-return
-
-end % % EOF
