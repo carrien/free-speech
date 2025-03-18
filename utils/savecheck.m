f796a7ddb7c7bdb4f841ad2ee42d367b5cd84fdb
Chris Naber <50926971+cwnaber@users.noreply.github.com>
2025-03-18 13:37:06 -0500

    add newlines in paths to fix TeX parsing (#143)

--- utils/savecheck.m
+++ utils/savecheck.m
@@ -17,6 +17,25 @@ if exist(savefile, ftype)
 
     colored_nameExt = sprintf('%s%s%s%s', '\color{blue}', name, extension, '\color{black}');
 
+    % To fix issue with `textwrap` in `questdlg` where TeX commands can be
+    % split across lines after 75 characters, proactively add some newlines
+    if length(path) > 75
+        new_path = '';
+        charCount = 0; % Counter to track characters since last newline
+
+        for i = 1:length(path)
+            new_path = [new_path, path(i)]; %#ok<AGROW> Append character
+            charCount = charCount + 1;
+
+            % If forward slash and near-ish the 75th character, add newline
+            if path(i) == '/' && charCount > 52
+                new_path = [new_path, newline]; %#ok<AGROW> Append newline
+                charCount = 0; % Reset character counter
+            end
+        end
+        path = new_path;
+    end
+
     % uses TeX formatting to change color and font size. See questdlg
     % documentation for help.
     messg = sprintf('%sThere is already a %s named %s here:\n\n%s\n\nDo you want to overwrite %s?', ...
