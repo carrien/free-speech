141f012f7bff9a38da9b0bcf01bced9846808805
Chris Naber <50926971+cwnaber@users.noreply.github.com>
2025-09-19 14:30:56 -0500

    delete old code; adjust comments; reorganize

--- experiment_helpers/setup_exptFigs.m
+++ experiment_helpers/setup_exptFigs.m
@@ -1,9 +1,9 @@
 function [h_fig] = setup_exptFigs()
 %SETUP_EXPTFIGS  Creates figures for experimenter and participant.
 %   SETUP_EXPTFIGS() creates and returns handles to figures for:
-%   (1) displaying stimuli to the participant (monitor 2)
-%   (2) displaying status and formant tracks for experimenter (monitor 1)
-%   (3) displaying a copy of the participant's view for experimenter (monitor 1)
+%   (1) displaying stimuli to the participant (second-from-the-left monitor)
+%   (2) displaying status and formant tracks for experimenter (leftmost monitor)
+%   (3) displaying a copy of the participant's view for experimenter (leftmost monitor)
 
 % set figure positions
 get_figinds_audapter;
@@ -15,11 +15,11 @@ pos{ctrl} = [0.6 0.2 0.4 0.8]; % experimenter view and formant tracking. Display
 pos{dup} = [0 0.3 0.6 0.7];    % duplicate of 'stim' for monitoring. Displays on experimenter's monitor.
 
 % Regardless of how the experimenter and participant's monitors are
-% PHYSICALLY positioned (left vs right), this code assumes that in Windows
+% PHYSICALLY positioned, this code assumes that in Windows
 % display settings, the experimenter's screen is virtually positioned to
-% the leftmost position, and the participant's screen is to the right of
+% the leftmost position, and the participant's screen is immediately to the right of
 % the experimenter's screen. The leftmost monitor's position will have the 
-% smallest x position value. That x coordinate value may be 1, or it may be
+% smallest x-coordinate value. That x coordinate value may be 1, or it may be
 % a negative number, depending on factors outside of your direct control.
 %
 % Each row in get(0, 'MonitorPositions') represents a monitor. Which row a
@@ -44,28 +44,11 @@ if nMonitors > 1
     % adjust each figure's position depending on which monitor it's
     % supposed to display on.
     exptMon_relativePos = round((exptMon_xCoord-1) / exptMon_xLength);
+    ppMon_relativePos   = round((ppMon_xCoord-1)   / ppMon_xLength);
+    pos{stim}(1) = pos{stim}(1) + ppMon_relativePos;
     pos{ctrl}(1) = pos{ctrl}(1) + exptMon_relativePos;
     pos{dup}(1)  = pos{dup}(1)  + exptMon_relativePos;
-
-    ppMon_relativePos = round((ppMon_xCoord-1) / ppMon_xLength);
-    pos{stim}(1) = pos{stim}(1) + ppMon_relativePos;
 end
-    
-
-%     monitor1_xCoordinate = monitorPositions(1,1);
-%     monitor2_xCoordinate = monitorPositions(2,1);
-%     mon2LeftOfMon1 = monitor2_xCoordinate < monitor1_xCoordinate;
-%     if mon2LeftOfMon1
-%         pos{stim} = [-1 0 1 1];         % participant view on left screen
-%     else
-%         pos{stim} = [1 0 1 1];         % participant view on right screen
-%     end
-% else %1 monitor setup
-%     pos{stim} = [1 0 1 1];
-% end
-% 
-% pos{ctrl} = [0.6 0.2 0.4 0.8]; % experimenter view and formant tracking
-% pos{dup} = [0 0.3 0.6 0.7];    % duplicate participant view for experimenter
 
 % define figure attributes
 figparams.Units = 'normalized';
