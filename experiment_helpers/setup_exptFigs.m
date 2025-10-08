da70ad1bed8f815d06d0955965a7d1ad8ff0df58
Chris Naber <50926971+cwnaber@users.noreply.github.com>
2025-10-08 09:08:40 -0500

    Change to logic for picking which monitor an experiment-running figure displays on (#155)
    * Set screen 2 relative to screen 1
    
    * Experimenter monitor anchored on left
    
    * Fix order of operations
    
    * delete old code; adjust comments; reorganize
    
    * setup_exptFigs add comments

--- experiment_helpers/setup_exptFigs.m
+++ experiment_helpers/setup_exptFigs.m
@@ -1,23 +1,54 @@
 function [h_fig] = setup_exptFigs()
 %SETUP_EXPTFIGS  Creates figures for experimenter and participant.
 %   SETUP_EXPTFIGS() creates and returns handles to figures for:
-%   (1) displaying stimuli to the participant (monitor 2)
-%   (2) displaying status and formant tracks for experimenter (monitor 1)
-%   (3) displaying a copy of the participant's view for experimenter (monitor 1)
+%   (1) displaying stimuli to the participant (second-from-the-left screen in Windows display settings)
+%   (2) displaying status and formant tracks for experimenter (leftmost screen in Windows display settings)
+%   (3) displaying a copy of the participant's view for experimenter (leftmost screen in Windows display settings)
 
 % set figure positions
 get_figinds_audapter;
 
-% check if 2nd monitor is to the right (pos. value) or left (neg. value) of main screen
-monitorPositions = get(0, 'MonitorPositions');
-if height(monitorPositions) > 1 && monitorPositions(2,1) < 0
-    pos{stim} = [-1 0 1 1];         % participant view on left screen
-else
-    pos{stim} = [1 0 1 1];         % participant view on right screen
-end
+% These are the baseline positions of each figure. The x position (first
+% value) is adjusted later once we know where the monitors are.
+pos{stim} = [0 0 1 1];         % stimulus presentation window. Displays on participant's monitor.
+pos{ctrl} = [0.6 0.2 0.4 0.8]; % experimenter view and formant tracking. Displays on experimenter's monitor.
+pos{dup} = [0 0.3 0.6 0.7];    % duplicate of 'stim' for monitoring. Displays on experimenter's monitor.
 
-pos{ctrl} = [0.6 0.2 0.4 0.8]; % experimenter view and formant tracking
-pos{dup} = [0 0.3 0.6 0.7];    % duplicate participant view for experimenter
+% Regardless of how the experimenter and participant's monitors are
+% PHYSICALLY positioned, this code assumes that in Windows
+% display settings, the experimenter's screen is virtually positioned to
+% the leftmost position, and the participant's screen is immediately to the right of
+% the experimenter's screen. The leftmost monitor's position will have the 
+% smallest x-coordinate value. That x coordinate value may be 1, or it may be
+% a negative number, depending on factors outside of your direct control.
+%
+% Each row in get(0, 'MonitorPositions') represents a monitor. Which row a
+% monitor appears in is also outside of your direct control. Therefore, we
+% have to do a lookup of the x position to determine which indexed monitor
+% is the leftmost one. That will be set to the experimenter's monitor, and
+% the next monitor to the right will be the participant's monitor's index.
+
+monitorPositions = get(0, 'MonitorPositions');
+nMonitors = height(monitorPositions);
+if nMonitors > 1
+    [~, sortedMonitorsIndices] = sort(monitorPositions); % sorts rows from lowest to highest x coordinate value
+
+    % get values for experimenter and participant monitors
+    exptMon_ix = sortedMonitorsIndices(1);
+    exptMon_xCoord = monitorPositions(exptMon_ix, 1);
+    exptMon_xLength = monitorPositions(exptMon_ix, 3);
+    ppMon_ix = sortedMonitorsIndices(2);
+    ppMon_xCoord = monitorPositions(ppMon_ix, 1);
+    ppMon_xLength = monitorPositions(ppMon_ix, 3);
+
+    % adjust each figure's position depending on which monitor it's
+    % supposed to display on.
+    exptMon_relativePos = round((exptMon_xCoord-1) / exptMon_xLength);
+    ppMon_relativePos   = round((ppMon_xCoord-1)   / ppMon_xLength);
+    pos{stim}(1) = pos{stim}(1) + ppMon_relativePos;
+    pos{ctrl}(1) = pos{ctrl}(1) + exptMon_relativePos;
+    pos{dup}(1)  = pos{dup}(1)  + exptMon_relativePos;
+end
 
 % define figure attributes
 figparams.Units = 'normalized';
