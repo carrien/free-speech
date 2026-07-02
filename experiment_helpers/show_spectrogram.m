7091dd22a4bae41380afa828a99b500490594fda
Chris Naber <50926971+cwnaber@users.noreply.github.com>
2026-07-02 13:38:19 -0500

    embed fsic to not rely on audapter_matlab

--- experiment_helpers/show_spectrogram.m
+++ experiment_helpers/show_spectrogram.m
@@ -41,4 +41,32 @@ max_vals = max(log10(abs(s)));
 caxis([min(min_vals(~isinf(min_vals)))-10 max(max_vals(~isinf(max_vals)))+10])
 
 
-return
+end
+
+
+function index =  fsic(theCell, theString, varargin)
+% findStringInCell: Find indices to all instances of a string in a cell array
+%
+% Author: Shanqing Cai (shanqing.cai@gmail.com, https://github.com/shanqing-cai/commonmcode)
+index = [];
+
+inverse=0;
+if (nargin>2)
+    if (isequal(varargin{1},'inv') || isequal(varargin{1},'inverse'))
+        inverse=1;
+    end
+end
+for n = 1:length(theCell)
+    if (~isstruct(theCell{n}))
+        if (~inverse)
+            if (strcmp(theCell{n}, theString))
+                index = [index, n];
+            end
+        else
+            if (~strcmp(theCell{n}, theString))
+                index = [index, n];
+            end
+        end
+    end
+end
+end
